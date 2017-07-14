35e0ffd1a0364e0af3a82aa7515827144cf4bc27
Shyam Chen <shyamchen1994@gmail.com>
2017-07-14 21:34:12 +0800

    2nd Commit

---
+++ src/pages/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <p class="text">{{ text }}</p>
+</template>
+
+<style>
+  .text {
+    color: blue;
+  }
+</style>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      text: 'About Component'
+    }
+  }
+}
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <p class="text">{{ text }}</p>
+</template>
+
+<style>
+  .text {
+    color: red;
+  }
+</style>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      text: 'Home Component'
+    }
+  }
+}
+</script>
