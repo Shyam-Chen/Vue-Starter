10dc32f3bd8b60b67beab5844d71cbe4c23f9786
Shyam Chen <shyamchen1994@gmail.com>
2017-07-30 15:47:45 +0800

    8th Commit

--- src/components/App.vue
+++ src/components/App.vue
@@ -13,11 +13,11 @@
     <input type="text" v-model="thing">
 
     <div>
-      <button v-on:click="counter++">Increment</button>
+      <button @click="counter++">Increment</button>
       <span>Clicked: {{ counter }} times</span>
     </div>
 
-    <div v-bind:class="{ active: isActive }">Vue + Vuex</div>
+    <div :class="{ active: isActive }">Vue + Vuex</div>
 
     <button @click="onLog">Click Me (Log)</button>
 
@@ -25,6 +25,53 @@
     <about></about>
 
     <counter></counter>
+
+    <md-button class="md-primary">Primary</md-button>
+    <md-button class="md-accent">Accent</md-button>
+
+    <md-table-card>
+      <md-toolbar>
+        <h1 class="md-title">Nutrition</h1>
+        <md-button class="md-icon-button">
+          <md-icon>filter_list</md-icon>
+        </md-button>
+
+        <md-button class="md-icon-button">
+          <md-icon>search</md-icon>
+        </md-button>
+      </md-toolbar>
+
+      <md-table md-sort="dessert" md-sort-type="desc" @select="onSelect" @sort="onSort">
+        <md-table-header>
+          <md-table-row>
+            <md-table-head md-sort-by="dessert">Dessert (100g serving)</md-table-head>
+            <md-table-head md-sort-by="calories" md-numeric md-tooltip="The total amount of food energy and the given serving size">Calories (g)</md-table-head>
+            <md-table-head md-sort-by="fat" md-numeric>Fat (g)</md-table-head>
+            <md-table-head>
+              <md-icon>message</md-icon>
+              <span>Comments</span>
+            </md-table-head>
+          </md-table-row>
+        </md-table-header>
+
+        <md-table-body>
+          <md-table-row v-for="(row, rowIndex) in nutrition" :key="rowIndex" :md-item="row" md-auto-select md-selection>
+            <md-table-cell v-for="(column, columnIndex) in row" :key="columnIndex" :md-numeric="columnIndex !== 'dessert' && columnIndex !== 'comment'" v-if="columnIndex !== 'type'">
+              {{ column }}
+            </md-table-cell>
+          </md-table-row>
+        </md-table-body>
+      </md-table>
+
+      <md-table-pagination
+        md-size="5"
+        md-total="10"
+        md-page="1"
+        md-label="Rows"
+        md-separator="of"
+        :md-page-options="[5, 10, 25, 50]"
+        @pagination="onPagination"></md-table-pagination>
+    </md-table-card>
   </div>
 </template>
 
@@ -50,6 +97,40 @@ export default {
       counter: 0,
 
       isActive: true,
+
+      nutrition: [
+        {
+          dessert: 'Frozen yogurt',
+          type: 'ice_cream',
+          calories: '159',
+          fat: '6.0',
+          comment: 'Icy'
+        }, {
+          dessert: 'Ice cream sandwich',
+          type: 'ice_cream',
+          calories: '237',
+          fat: '9.0',
+          comment: 'Super Tasty'
+        }, {
+          dessert: 'Eclair',
+          type: 'pastry',
+          calories: '262',
+          fat: '16.0',
+          comment: ''
+        }, {
+          dessert: 'Cupcake',
+          type: 'pastry',
+          calories: '305',
+          fat: '3.7',
+          comment: ''
+        }, {
+          dessert: 'Gingerbread',
+          type: 'other',
+          calories: '356',
+          fat: '16.0',
+          comment: ''
+        }
+      ],
     }
   },
   methods: {
@@ -65,11 +146,10 @@ export default {
 <style>
 .message {
   color: pink;
-  font-size: 1.4em;
 }
 
 .active {
-  color: blue;
+  color: red;
 }
 </style>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+import material from 'vue-material';
 
 import App from './components/App.vue';
 
 import store from './root';
+
+Vue.use(material)
 
 new Vue({
   el: '#app',
