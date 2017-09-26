b17efcd5927f1781926cc9aea5f66e810c373917
Shyam Chen <shyamchen1994@gmail.com>
2017-09-26 13:24:57 +0800

    33rd Commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,46 @@
 const { join } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const BABEL_LOADER = {
+  loader: 'babel-loader',
+  options: {
+    babelrc: false,
+    presets: [
+      [
+        'env', {
+          'targets': {
+            'browsers': ['last 2 versions']
+          }
+        }
+      ]
+    ],
+    plugins: [
+      'transform-runtime',
+      'transform-function-bind',
+      'transform-object-rest-spread',
+      [
+        'transform-imports', {
+          'rxjs': {
+            transform: 'rxjs/${member}',
+            preventFullImport: true,
+            skipDefaultConversion: true
+          },
+          'rxjs/observable': {
+            transform: 'rxjs/observable/${member}',
+            preventFullImport: true,
+            skipDefaultConversion: true
+          },
+          'rxjs/operator': {
+            transform: 'rxjs/operator/${member}',
+            preventFullImport: true,
+            skipDefaultConversion: true
+          }
+        }
+      ]
+    ]
+  }
+};
+
 module.exports = {
   context: join(__dirname, 'src'),
   entry: {
@@ -16,45 +56,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              babelrc: false,
-              presets: [
-                [
-                  'env', {
-                    'targets': {
-                      'browsers': ['last 2 versions']
-                    }
-                  }
-                ]
-              ],
-              plugins: [
-                'transform-runtime',
-                'transform-function-bind',
-                'transform-object-rest-spread',
-                [
-                  'transform-imports', {
-                    'rxjs': {
-                      transform: 'rxjs/${member}',
-                      preventFullImport: true,
-                      skipDefaultConversion: true
-                    },
-                    'rxjs/observable': {
-                      transform: 'rxjs/observable/${member}',
-                      preventFullImport: true,
-                      skipDefaultConversion: true
-                    },
-                    'rxjs/operator': {
-                      transform: 'rxjs/operator/${member}',
-                      preventFullImport: true,
-                      skipDefaultConversion: true
-                    }
-                  }
-                ]
-              ]
-            }
-          }
+          BABEL_LOADER
         ]
       }, {
         test: /\.css$/,
@@ -74,45 +76,7 @@ module.exports = {
             loader: 'vue-loader',
             options: {
               loaders: {
-                js: {
-                  loader: 'babel-loader',
-                  options: {
-                    babelrc: false,
-                    presets: [
-                      [
-                        'env', {
-                          'targets': {
-                            'browsers': ['last 2 versions']
-                          }
-                        }
-                      ]
-                    ],
-                    plugins: [
-                      'transform-runtime',
-                      'transform-function-bind',
-                      'transform-object-rest-spread',
-                      [
-                        'transform-imports', {
-                          'rxjs': {
-                            transform: 'rxjs/${member}',
-                            preventFullImport: true,
-                            skipDefaultConversion: true
-                          },
-                          'rxjs/observable': {
-                            transform: 'rxjs/observable/${member}',
-                            preventFullImport: true,
-                            skipDefaultConversion: true
-                          },
-                          'rxjs/operator': {
-                            transform: 'rxjs/operator/${member}',
-                            preventFullImport: true,
-                            skipDefaultConversion: true
-                          }
-                        }
-                      ]
-                    ]
-                  }
-                }
+                js: BABEL_LOADER
               }
             }
           }
