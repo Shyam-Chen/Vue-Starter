a8ec5d18bf241ebc39af5fdca2f03c739e257d6d
Shyam Chen <shyamchen1994@gmail.com>
2017-10-04 14:08:51 +0800

    47th Commit

--- src/app/store.js
+++ src/app/store.js
@@ -4,13 +4,18 @@ import Vuex from 'vuex';
 import { counter } from './counter';
 import { rest } from './rest';
 
+import { INITIAL as state } from './constants';
+import actions from './actions';
+import mutations from './mutations';
+import getters from './getters';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  actions: {},
-  mutations: {},
-  getters: {},
+  state,
+  actions,
+  mutations,
+  getters,
   modules: {
     counter,
     rest
