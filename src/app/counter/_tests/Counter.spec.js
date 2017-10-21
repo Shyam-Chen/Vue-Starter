200fddc203bdedc1b9aa37acc4e46d0d4dda6949
Shyam Chen <shyamchen1994@gmail.com>
2017-10-21 15:08:36 +0800

    54th Commit

---
+++ src/app/counter/_tests/Counter.spec.js
@@ -0,0 +1,15 @@
+// import { shallow } from 'vue-test-utils';
+
+import Counter from '../Counter';
+
+describe('Counter', () => {
+  // let component;
+
+  beforeEach(() => {
+    // component = shallow(Counter, {});
+  });
+
+  it('should render initial component', () => {
+    expect(Counter).toBeDefined();
+  });
+});
