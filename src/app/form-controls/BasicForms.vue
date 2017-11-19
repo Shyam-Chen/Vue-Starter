b20aaba0ef6b936b75dde54100941a9004375c04
Shyam Chen <shyamchen1994@gmail.com>
2017-11-19 16:58:25 +0800

    73rd Commit

--- src/app/form-controls/BasicForms.vue
+++ src/app/form-controls/BasicForms.vue
@@ -1,11 +1,59 @@
 <template>
   <div class="container">
     <h1>Basic Forms</h1>
+
+    <md-whiteframe md-elevation="2">
+      <form class="form">
+        <div class="row">
+          <!-- input -->
+          <md-input-container class="input">
+            <label>Nickname</label>
+            <md-input v-model="$store.state.formControls.nickname"></md-input>
+          </md-input-container>
+          <div class="outputs">
+            {{ $store.state.formControls.nickname }}
+          </div>
+        </div>
+
+        <div class="row">
+          <div>
+            <div class="md-body-2">Gender</div>
+            <md-radio v-model="$store.state.formControls.gender" name="gender" md-value="Male">Male</md-radio>
+            <md-radio v-model="$store.state.formControls.gender" name="gender" md-value="Female">Female</md-radio>
+            <md-radio v-model="$store.state.formControls.gender" name="gender" md-value="Other">Other</md-radio>
+          </div>
+          <div class="outputs" style="padding-top: 2rem">
+            {{ $store.state.formControls.gender }}
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="md-body-2" style="align-self: center">Autoplay</div>
+          <div style="margin-left: 1rem">
+            <md-switch v-model="autoplay" name="autoplay"></md-switch>
+          </div>
+          <div class="outputs" style="padding-top: .5rem">
+            {{ autoplay ? `${autoplay}`.charAt(0).toUpperCase() + `${autoplay}`.slice(1) : '' }}
+          </div>
+        </div>
+
+        <div class="row">
+          ...
+        </div>
+      </form>
+    </md-whiteframe>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    autoplay: {
+      get() { return this.$store.state.formControls.autoplay; },
+      set(autoplay) { this.$store.commit('autoplay', { autoplay }); }
+    }
+  },
+};
 </script>
 
 <style scoped>
@@ -13,5 +61,26 @@ export default {};
   background: #FAFAFA;
   height: 100vh;
   padding: 1rem;
+}
+
+.form {
+  padding: 1rem;
+  background: #FFFFFF;
+}
+
+.row {
+  padding: .66rem;
+  display: flex;
+  flex-direction: row;
+}
+
+.input {
+  max-width: 10rem;
+}
+
+.outputs {
+  align-self: center;
+  margin: 0 0 .5rem .5rem;
+  color: #009688;
 }
 </style>
--- src/app/form-controls/FormControls.vue
+++ src/app/form-controls/FormControls.vue
@@ -2,65 +2,26 @@
   <div class="container">
     <Navigation />
 
-    <router-link to="/form-controls/basic-forms" tag="md-button" class="md-raised md-accent">Basic Forms</router-link>
-    <router-link to="/form-controls/reactive-forms" tag="md-button" class="md-raised md-accent">Reactive Forms</router-link>
+    <router-link to="/form-controls/basic-forms" name="basicForms" tag="md-button" class="md-raised md-accent">Basic Forms</router-link>
+    <router-link to="/form-controls/reactive-forms" name="reactiveForms" tag="md-button" class="md-raised md-accent">Reactive Forms</router-link>
 
     <router-view></router-view>
-
-    <md-whiteframe md-elevation="2">
-      <form class="form">
-        <div class="row">
-          <!-- input -->
-          <md-input-container class="input">
-            <label>Nickname</label>
-            <md-input v-model="$store.state.formControls.nickname"></md-input>
-          </md-input-container>
-          <div class="outputs">
-            {{ $store.state.formControls.nickname }}
-          </div>
-        </div>
-
-        <div class="row">
-          <div>
-            <div class="md-body-2">Gender</div>
-            <md-radio v-model="$store.state.formControls.gender" name="gender" md-value="Male">Male</md-radio>
-            <md-radio v-model="$store.state.formControls.gender" name="gender" md-value="Female">Female</md-radio>
-            <md-radio v-model="$store.state.formControls.gender" name="gender" md-value="Other">Other</md-radio>
-          </div>
-          <div class="outputs" style="padding-top: 2rem">
-            {{ $store.state.formControls.gender }}
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="md-body-2" style="align-self: center">Autoplay</div>
-          <div style="margin-left: 1rem">
-            <md-switch v-model="autoplay" name="autoplay"></md-switch>
-          </div>
-          <div class="outputs" style="padding-top: .5rem">
-            {{ autoplay ? `${autoplay}`.charAt(0).toUpperCase() + `${autoplay}`.slice(1) : '' }}
-          </div>
-        </div>
-
-        <div class="row">
-          ...
-        </div>
-      </form>
-    </md-whiteframe>
   </div>
 </template>
 
 <script>
 import Navigation from '~/shared/Navigation';
 
+import BasicForms from './BasicForms';
+import ReactiveForms from './ReactiveForms';
+
 export default {
-  computed: {
-    autoplay: {
-      get() { return this.$store.state.formControls.autoplay; },
-      set(autoplay) { this.$store.commit('autoplay', { autoplay }); }
-    }
-  },
-  components: { Navigation }
+  components: {
+    Navigation,
+
+    BasicForms,
+    ReactiveForms
+  }
 };
 </script>
 
@@ -69,26 +30,5 @@ export default {
   background: #FAFAFA;
   height: 100vh;
   padding: 1rem;
-}
-
-.form {
-  padding: 1rem;
-  background: #FFFFFF;
-}
-
-.row {
-  padding: .66rem;
-  display: flex;
-  flex-direction: row;
-}
-
-.input {
-  max-width: 10rem;
-}
-
-.outputs {
-  align-self: center;
-  margin: 0 0 .5rem .5rem;
-  color: #009688;
 }
 </style>
