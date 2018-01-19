93ec8a46e7a65408dbae2655fe5d01ddaa3cf46d
Shyam-Chen <shyamchen1994@gmail.com>
2018-01-19 18:09:57 +0800

    104th Commit

--- src/client.js
+++ src/client.js
@@ -1,11 +1,13 @@
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
+import 'vuetify/dist/vuetify.min.css';
 
 import './assets/styles/global.css';
 
 import Vue from 'vue';
 import { sync } from 'vuex-router-sync';
 import material from 'vue-material';
+import Vuetify from 'vuetify';
 import reactivex from 'vue-rx';
 import { Observable } from 'rxjs';
 // import Apollo from 'vue-apollo';
@@ -17,6 +19,7 @@ import { router, store } from './app';
 sync(store, router);
 
 Vue.use(material);
+Vue.use(Vuetify);
 Vue.use(reactivex, { Observable });
 // Vue.use(Apollo);
 Vue.use(I18n);
