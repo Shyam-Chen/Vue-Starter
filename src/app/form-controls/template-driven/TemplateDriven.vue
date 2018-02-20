201ac0b0ac1c0fa64b297a55ba527d06d551b61e
Shyam Chen <shyamchen1994@gmail.com>
2018-02-20 19:37:01 +0800

    116th Commit

--- src/app/form-controls/template-driven/TemplateDriven.vue
+++ src/app/form-controls/template-driven/TemplateDriven.vue
@@ -64,13 +64,11 @@
 <script>
 export const mapModelsToState = (state, keys) => {
   const obj = {};
+  const arr = state.split('.');
 
   for (let i = 0, l = keys.length; i < l; i++) {
     obj[keys[i]] = {
       get() {
-        const arr = state.split('.');
-
-        // TODO: repeat
         if (arr.length === 1) {
           return this.$store.state[arr[0]][keys[i]];
         }
@@ -78,14 +76,19 @@ export const mapModelsToState = (state, keys) => {
         if (arr.length === 2) {
           return this.$store.state[arr[0]][arr[1]][keys[i]];
         }
-
-        if (arr.length === 3) {
-          return this.$store.state[arr[0]][arr[1]][arr[2]][keys[i]];
-        }
       },
       set(value) {
-        // TODO: scope
-        this.$store.commit(`update`, { label: [keys[i]], value });
+        if (arr.length === 1) {
+          const module = arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
+          this.$store.commit(`update${module}`, { label: [keys[i]], value });
+        }
+
+        if (arr.length === 2) {
+          const moduleParent = arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
+          const moduleChild = arr[1].charAt(0).toUpperCase() + arr[1].slice(1);
+
+          this.$store.commit(`update${moduleParent}${moduleChild}`, { label: [keys[i]], value });
+        }
       }
     };
   }
--- src/app/form-controls/template-driven/mutations.js
+++ src/app/form-controls/template-driven/mutations.js
@@ -1,10 +1,28 @@
-export const updateModel = () => ({
-  // TODO: scope
-  [`update`](state, { label, value }) {
-    state[label] = value;
+export const updateModel = commitName => {
+  const arr = commitName.split('.');
+
+  if (arr.length === 1) {
+    const module = arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
+
+    return {
+      [`update${module}`](state, { label, value }) {
+        state[label] = value;
+      }
+    };
   }
-})
+
+  if (arr.length === 2) {
+    const moduleParent = arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
+    const moduleChild = arr[1].charAt(0).toUpperCase() + arr[1].slice(1);
+
+    return {
+      [`update${moduleParent}${moduleChild}`](state, { label, value }) {
+        state[label] = value;
+      }
+    };
+  }
+};
 
 export default {
-  ...updateModel()
+  ...updateModel('formControls.templateDriven')
 };
