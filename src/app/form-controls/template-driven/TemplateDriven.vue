2e3de393836eb86d2660ad480e0b9334e52b4f8c
Shyam-Chen <shyamchen1994@gmail.com>
2018-02-21 10:45:59 +0800

    117th Commit

--- src/app/form-controls/template-driven/TemplateDriven.vue
+++ src/app/form-controls/template-driven/TemplateDriven.vue
@@ -62,39 +62,7 @@
 </template>
 
 <script>
-export const mapModelsToState = (state, keys) => {
-  const obj = {};
-  const arr = state.split('.');
-
-  for (let i = 0, l = keys.length; i < l; i++) {
-    obj[keys[i]] = {
-      get() {
-        if (arr.length === 1) {
-          return this.$store.state[arr[0]][keys[i]];
-        }
-
-        if (arr.length === 2) {
-          return this.$store.state[arr[0]][arr[1]][keys[i]];
-        }
-      },
-      set(value) {
-        if (arr.length === 1) {
-          const module = arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
-          this.$store.commit(`update${module}`, { label: [keys[i]], value });
-        }
-
-        if (arr.length === 2) {
-          const moduleParent = arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
-          const moduleChild = arr[1].charAt(0).toUpperCase() + arr[1].slice(1);
-
-          this.$store.commit(`update${moduleParent}${moduleChild}`, { label: [keys[i]], value });
-        }
-      }
-    };
-  }
-
-  return obj;
-};
+import { mapModelsToState } from 'vuex-bound';
 
 export default {
   computed: {
--- src/app/form-controls/template-driven/mutations.js
+++ src/app/form-controls/template-driven/mutations.js
@@ -1,27 +1,4 @@
-export const updateModel = commitName => {
-  const arr = commitName.split('.');
-
-  if (arr.length === 1) {
-    const module = arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
-
-    return {
-      [`update${module}`](state, { label, value }) {
-        state[label] = value;
-      }
-    };
-  }
-
-  if (arr.length === 2) {
-    const moduleParent = arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
-    const moduleChild = arr[1].charAt(0).toUpperCase() + arr[1].slice(1);
-
-    return {
-      [`update${moduleParent}${moduleChild}`](state, { label, value }) {
-        state[label] = value;
-      }
-    };
-  }
-};
+import { updateModel } from 'vuex-bound';
 
 export default {
   ...updateModel('formControls.templateDriven')
