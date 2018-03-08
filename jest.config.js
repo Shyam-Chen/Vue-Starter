37e5c3c2301c6636caf20a8803206bc844aa694a
Shyam-Chen <shyamchen1994@gmail.com>
2018-03-08 16:31:40 +0800

    139th Commit

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   moduleNameMapper: {
-    '^vue$': 'vue/dist/vue.common.js',
     '~(.*)': '<rootDir>/src/app$1',
   },
   moduleFileExtensions: ['js', 'vue'],
+  snapshotSerializers: [
+    '<rootDir>/node_modules/jest-serializer-vue',
+  ],
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
