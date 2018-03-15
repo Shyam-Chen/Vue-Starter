15397f841b039719f12bf854a369e71c368f4c43
Shyam-Chen <shyamchen1994@gmail.com>
2018-03-15 11:55:36 +0800

    165th Commit

--- src/app/shared/Overview.vue
+++ src/app/shared/Overview.vue
@@ -2,14 +2,41 @@
 export default {
   render() {
     return (
-      <div>
-        <v-btn color="info" to="/counter">Counter</v-btn>
-        <v-btn color="info" to="/basic">CRUD</v-btn>
-        <v-btn color="info" to="/rest">REST</v-btn>
-        <v-btn color="info" disabled to="/graphql">GraphQL</v-btn>
-        <v-btn color="info" to="/form-controls">Form Controls</v-btn>
-        <v-btn color="info" to="/i18n">I18n</v-btn>
-      </div>
+      <v-layout column>
+        <v-layout column>
+          <div class="headline">CRUD Operations</div>
+          <div>
+            <v-btn color="info" to="/basic">CRUD</v-btn>
+            <v-btn color="info" to="/rest">REST</v-btn>
+            <v-btn color="info" disabled to="/graphql">GraphQL</v-btn>
+          </div>
+        </v-layout>
+
+        <v-layout column>
+          <div class="headline">Form Controls</div>
+          <div>
+            <v-btn color="info" to="/form-controls">Form Controls</v-btn>
+            {' - '}
+            <v-btn color="info" to="/form-controls/template-driven">Template-driven</v-btn>
+            <v-btn color="info" disabled to="/form-controls/reactive">Reactive</v-btn>
+          </div>
+        </v-layout>
+
+        <v-layout column>
+          <div class="headline">Globalization</div>
+          <div>
+            <v-btn color="info" to="/i18n">I18n</v-btn>
+          </div>
+        </v-layout>
+
+        <v-layout column>
+          <div class="headline">Playground</div>
+          <div>
+            <v-btn color="info" to="/counter">Counter</v-btn>
+          </div>
+        </v-layout>
+
+      </v-layout>
     );
   },
 };
