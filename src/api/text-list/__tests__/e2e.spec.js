075a2f7f7462939b452439502f2fd157199dd4ec
Shyam-Chen <shyamchen1994@gmail.com>
2018-03-20 13:06:34 +0800

    189th Commit

---
+++ src/api/text-list/__tests__/e2e.spec.js
@@ -0,0 +1,12 @@
+import request from 'supertest';
+
+import { FUNC_URL } from '../../../env';
+
+describe('Text List', () => {
+  it('should get a text list', async () => {
+    const { statusCode } = await request(FUNC_URL)
+      .get('/api/text-list');
+
+    expect(statusCode).toBe(200);
+  });
+});
