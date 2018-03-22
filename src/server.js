f2960840c87965af84052d33aaa91b069df763cb
Shyam-Chen <shyamchen1994@gmail.com>
2018-03-22 12:22:05 +0800

    205th Commit

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import express from 'express';
+import compression from 'compression';
 import cors from 'cors';
+import morgan from 'morgan';
 import bodyParser from 'body-parser';
 
 import routes from './api';
@@ -10,7 +12,9 @@ admin.initializeApp(functions.config().firebase);
 
 const app = express();
 
+app.use(compression());
 app.use(cors({ origin: true }));
+app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
