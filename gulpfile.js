7210d1b465aba4f7c2180a5d1425065978ab54c7
Shyam-Chen <shyamchen1994@gmail.com>
2018-03-26 17:37:41 +0800

    238th Commit

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('build', () =>
       '!src/assets', '!src/assets/**/*',
       '!src/**/__tests__', '!src/**/__tests__/**/*',
     ])
-    .pipe(!util.env.prod ? replaces(envify(env)) : util.noop())
+    .pipe(replaces(envify(env)))
     .pipe(babel())
     .pipe(gulp.dest('functions')),
 );
