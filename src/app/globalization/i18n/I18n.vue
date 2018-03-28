fb2a25f5fe512d70d7d1ba9ea41cde22463d95ee
Shyam-Chen <shyamchen1994@gmail.com>
2018-03-28 16:17:15 +0800

    253rd Commit

--- src/app/globalization/i18n/I18n.vue
+++ src/app/globalization/i18n/I18n.vue
@@ -12,7 +12,40 @@
     </v-layout>
 
     <v-layout row>
-      <p class="pa-3">{{ $t('message.hello') }}</p>
+      <p class="pl-2 pt-4">
+        HTML formatting:
+        <b class="indigo--text">{{ $t('bubble') }}</b>
+      </p>
+    </v-layout>
+
+    <v-layout row>
+      <p class="pl-2">
+        Named formatting:
+        <b class="indigo--text">{{ $t('hello', { msg: 'Vue' }) }}</b>
+      </p>
+    </v-layout>
+
+    <v-layout row>
+      <p class="pl-2">
+        List formatting:
+        <b class="indigo--text">{{ $t('thinkIn', ['Angular', 'React', 'Vue']) }}</b>
+      </p>
+    </v-layout>
+
+    <v-layout row>
+      <p class="pl-2">
+        Pluralization:
+        <b class="indigo--text">{{ $tc('http', 2) }}://google.com</b>
+      </p>
+    </v-layout>
+
+    <v-layout row>
+      <p class="pl-2">
+        Pluralizing i18n:
+        <b class="indigo--text">{{ $tc('framework', 0) }}</b>,
+        <b class="indigo--text">{{ $tc('framework', 1) }}</b>,
+        <b class="indigo--text">{{ $tc('framework', 3, { num: 3 }) }}</b>
+      </p>
     </v-layout>
 
   </v-layout>
