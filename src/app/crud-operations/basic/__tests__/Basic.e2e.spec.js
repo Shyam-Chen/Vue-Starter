079591353683b2e48126ec8fe5d4e26b8194940e
Shyam-Chen <shyamchen1994@gmail.com>
2018-03-31 16:38:11 +0800

    274th Commit

--- src/app/crud-operations/basic/__tests__/Basic.e2e.spec.js
+++ src/app/crud-operations/basic/__tests__/Basic.e2e.spec.js
@@ -45,22 +45,40 @@ describe('Basic', () => {
 
     await page.type(searchInput, 'v');
     const length = await page.$$eval(displayRow, el => el.length);
+
     expect(length).toBe(2);
   });
 
-  it('should delete selected item', () => {
+  it('should delete selected item', async () => {
+    const selectAll = '#app > div.application--wrap > main > div > div > div > div:nth-child(3) > div > div:nth-child(2) > div > table > thead > tr:nth-child(1) > th:nth-child(1) > div > div.input-group__input > div';
+    const deleteIcon = '#app > div.application--wrap > main > div > div > div > div:nth-child(3) > div > div.card__title.pink.lighten-4.vfs-card-title > button > div';
 
+    await page.click(selectAll);
+    await page.click(deleteIcon);
+    const length = await page.$$eval(displayRow, el => el.length);
+
+    expect(length).toBe(1);
   });
 
-  it('should edit a item', () => {
+  it('should edit a item', async () => {
+    const firstEditIcon = '#app > div.application--wrap > main > div > div > div > div:nth-child(3) > div > div:nth-child(2) > div > table > tbody > tr:nth-child(1) > td.text-xs-right > button:nth-child(1) > div';
+    const primaryInput = '#app > div.dialog__content.dialog__content__active > div > div > div.card__text > div > div:nth-child(1) > div > div.input-group__input > input[type="text"]';
+    const saveButton = '#app > div.dialog__content.dialog__content__active > div > div > div.card__actions > button.btn.btn--flat.green--text > div';
+    const firstPrimary = '#app > div.application--wrap > main > div > div > div > div:nth-child(3) > div > div:nth-child(2) > div > table > tbody > tr:nth-child(1) > td:nth-child(3)';
 
+    await page.click(firstEditIcon);
+    await page.type(primaryInput, ' foo');
+    await page.click(saveButton);
+    const text = await page.$eval(firstPrimary, el => el.textContent);
+
+    expect(text).toBe('Vanilla foo');
   });
 
   it('should delete a item', async () => {
-    const deleteIcon = '#app > div.application--wrap > main > div > div > div > div:nth-child(3) > div > div:nth-child(2) > div > table > tbody > tr:nth-child(1) > td.text-xs-right > button:nth-child(2) > div';
+    const firstDeleteIcon = '#app > div.application--wrap > main > div > div > div > div:nth-child(3) > div > div:nth-child(2) > div > table > tbody > tr:nth-child(1) > td.text-xs-right > button:nth-child(2) > div';
     const confirm = '#app > div.dialog__content.dialog__content__active > div > div > div.card__actions > button.btn.btn--flat.red--text > div';
 
-    await page.click(deleteIcon);
+    await page.click(firstDeleteIcon);
     await page.click(confirm);
     const length = await page.$$eval(displayRow, el => el.length);
 
