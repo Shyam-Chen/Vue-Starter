e993d98de3d9b1b5eaef85ed8b586f3bde548983
Shyam Chen <shyamchen1994@gmail.com>
2018-04-21 15:44:23 +0800

    373rd Commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const SWPrecachePlugin = require('sw-precache-webpack-plugin');
 const RobotstxtPlugin = require('robotstxt-webpack-plugin').default;
 const envify = require('process-envify');
@@ -16,6 +15,7 @@ const SOURCE_ROOT = path.join(__dirname, 'src');
 const DIST_ROOT = path.join(__dirname, 'public');
 
 module.exports = ({ prod = false } = {}) => ({
+  mode: prod ? 'production' : 'development',
   context: SOURCE_ROOT,
   entry: {
     client: './client.js',
@@ -114,42 +114,7 @@ module.exports = ({ prod = false } = {}) => ({
       },
     ]),
     new webpack.DefinePlugin(envify(env)),
-
     !prod && new webpack.HotModuleReplacementPlugin(),
-    !prod && new webpack.NamedModulesPlugin(),
-    !prod && new webpack.NoEmitOnErrorsPlugin(),
-
-    prod && new UglifyJSPlugin({
-      uglifyOptions: {
-        compress: {
-          warnings: false,
-        },
-      },
-      sourceMap: true,
-      parallel: true,
-    }),
-    prod && new webpack.HashedModuleIdsPlugin(),
-    prod && new webpack.optimize.ModuleConcatenationPlugin(),
-    prod && new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks(module) {
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(path.join(__dirname, 'node_modules')) === 0
-        );
-      },
-    }),
-    prod && new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      minChunks: Infinity,
-    }),
-    prod && new webpack.optimize.CommonsChunkPlugin({
-      name: 'app',
-      async: 'vendor-async',
-      children: true,
-      minChunks: 3,
-    }),
     prod && new SWPrecachePlugin({
       cacheId: pkg.name,
       filename: 'service-worker.js',
@@ -160,6 +125,20 @@ module.exports = ({ prod = false } = {}) => ({
     }),
     prod && new RobotstxtPlugin(),
   ].filter(Boolean),
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          name: 'vendor',
+          chunks: 'initial',
+        },
+      },
+    },
+    runtimeChunk: {
+      name: 'manifest',
+    },
+  },
   devServer: {
     contentBase: DIST_ROOT,
     historyApiFallback: true,
