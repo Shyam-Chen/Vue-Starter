f541cf200a26fd072a24d24b3707de6e322302c5
Shyam Chen <shyamchen1994@gmail.com>
2018-05-01 12:54:45 +0800

    399th Commit

---
+++ src/app/authorization/Authorization.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container fluid>
+    <div class="headline">Authorization</div>
+  </v-container>
+</template>
+
+<script>
+// @flow
+
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ src/app/messages/Messages.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container fluid>
+    <div class="headline">Messages</div>
+  </v-container>
+</template>
+
+<script>
+// @flow
+
+export default {};
+</script>
+
+<style scoped>
+</style>
