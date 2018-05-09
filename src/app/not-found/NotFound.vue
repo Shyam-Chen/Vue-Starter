91f22ffc4ada513d4f2dfd68b9a233f77676d792
Shyam-Chen <shyamchen1994@gmail.com>
2018-05-09 16:47:11 +0800

    415th Commit

--- src/app/not-found/NotFound.vue
+++ src/app/not-found/NotFound.vue
@@ -1,20 +1,19 @@
+<template>
+  <v-container fluid fill-height>
+    <v-layout justify-center align-center>
+      <div class="text-xs-center">
+        <div class="display-2 primary--text pa-3">Whoops, 404!</div>
+        <div class="pa-2">The page you were looking for does not exist.</div>
+        <v-btn to="/" color="primary" outline>Get me out of here!</v-btn>
+      </div>
+    </v-layout>
+  </v-container>
+</template>
+
 <script>
 export default {
   metaInfo: {
     title: 'Page Not Found | Oh My Vue',
-  },
-  render() {
-    return (
-      <v-container fluid fill-height>
-        <v-layout justify-center align-center>
-          <div class="text-xs-center">
-            <div class="display-2 primary--text pa-3">Whoops, 404!</div>
-            <div class="pa-2">The page you were looking for does not exist.</div>
-            <v-btn to='/' color="primary" outline>Get me out of here!</v-btn>
-          </div>
-        </v-layout>
-      </v-container>
-    );
   },
 };
 </script>
