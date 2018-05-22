c56ef6eb5ae49422bc27a502de12a092fdd8bbc7
Shyam-Chen <shyamchen1994@gmail.com>
2018-05-22 17:02:48 +0800

    462nd Commit

--- src/app/overview/Overview.vue
+++ src/app/overview/Overview.vue
@@ -3,13 +3,13 @@
     <v-layout column>
       <div class="title">Practical Examples</div>
       <div>00. Hello World - A bare-bones Hello World example.</div>
-      <div>01. Guide - Structuring Vue, Vue Router, Vuex for the Application.</div>
+      <div>01. Guide - Structuring Vue, Vue Router, Vuex for applications.</div>
       <div>02. CRUD Operations - The four major functions implemented in applications.</div>
-      <div>03. Form Controls - Text Fields, Selects, Picker, File Upload, Markdown Editor</div>
+      <div>03. Form Controls - Text Fields, Selects, Picker, File Upload, Markdown Editor, Autocomplete</div>
       <div>04. Data Table - Paginator, Iterator</div>
       <div>05. Internationalization - Translation</div>
       <div>06. Data Visualization - Pie Chart, Bar Chart</div>
-      <div>07. Authorization - Email/Password, Google, Facebook, Twitter, Instagram, Line</div>
+      <div>07. Authorization - Email/Password, Google, Facebook, Twitter, Instagram, Line, Forgot Password</div>
       <div>08. Payment - Stripe</div>
       <div>09. Messages - Send Email, Cloud Messaging, Chat Messages</div>
       <div>10. Machine Learning - Label Images</div>
