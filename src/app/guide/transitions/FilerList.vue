6ab685884b28675e919e88ba6896fd614cde4079
Shyam Chen <shyamchen1994@gmail.com>
2018-06-02 13:26:28 +0800

    481st Commit

---
+++ src/app/guide/transitions/FilerList.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-layout column class="mt-1 mb-1">
+    <div class="title mb-2">Filer List</div>
+
+    <div>
+      <v-btn @click="type = 'all'">All</v-btn>
+      <v-btn @click="type = 'odd'">Odd</v-btn>
+      <v-btn @click="type = 'even'">Even</v-btn>
+    </div>
+
+    <transition-group tag="ul" name="fade-group" class="filer-list">
+      <li v-for="item in filerList" :key="item" class="filer-item">{{ item }}</li>
+    </transition-group>
+  </v-layout>
+</template>
+
+<script>
+// @flow
+
+type Data = {
+  type: string,
+  list: number[],
+};
+
+export default {
+  data(): Data {
+    return {
+      type: 'all',
+      list: Array(21).fill(1).map((x, y) => x + y),
+    };
+  },
+  computed: {
+    filerList(): number[] {
+      return this.list.filter((value) => {
+        if (this.type === 'odd') return value % 2 === 1;
+        if (this.type === 'even') return value % 2 === 0;
+        return this.list;
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.filer-list {
+  width: 21rem;
+  padding: 0;
+}
+
+.filer-item {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0.5rem;
+  width: 2.5rem;
+  height: 2.5rem;
+  color: #fff;
+  background: #009688;
+  filter: drop-shadow(0 0 0.25rem #00796b);
+}
+
+.fade-group-enter-active,
+.fade-group-leave-active,
+.fade-group-move {
+  transition: opacity 1s, transform 1s;
+}
+
+.fade-group-leave-active {
+  position: absolute;
+}
+
+.fade-group-enter {
+  opacity: 0;
+  transform: translateY(-1.25rem);
+}
+
+.fade-group-leave-to {
+  opacity: 0;
+  transform: translateY(1.25rem);
+}
+</style>
--- src/app/guide/transitions/Transitions.vue
+++ src/app/guide/transitions/Transitions.vue
@@ -16,41 +16,24 @@
       </transition>
     </v-layout>
 
-    <v-layout column class="mt-1">
-      <div>
-        <v-btn @click="type = 'all'">All</v-btn>
-        <v-btn @click="type = 'odd'">Odd</v-btn>
-        <v-btn @click="type = 'even'">Even</v-btn>
-      </div>
-
-      <transition-group tag="ul" name="fade-group" class="fade-list">
-        <li v-for="item in filerList" :key="item" class="fade-item">{{ item }}</li>
-      </transition-group>
-    </v-layout>
+    <FilerList />
   </v-container>
 </template>
 
 <script>
+import FilerList from './FilerList';
+
 export default {
+  components: {
+    FilerList,
+  },
   data() {
     return {
       fade: {
         transition: true,
         animation: true,
       },
-
-      type: 'all',
-      list: Array(21).fill(1).map((x, y) => x + y),
     };
-  },
-  computed: {
-    filerList() {
-      return this.list.filter((value) => {
-        if (this.type === 'odd') return value % 2 === 1;
-        if (this.type === 'even') return value % 2 === 0;
-        return this.list;
-      });
-    },
   },
 };
 </script>
@@ -84,43 +67,5 @@ export default {
   100% {
     opacity: 1;
   }
-}
-
-/* fade-group */
-.fade-list {
-  width: 21rem;
-  padding: 0;
-}
-
-.fade-item {
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  margin: 0.5rem;
-  width: 2.5rem;
-  height: 2.5rem;
-  color: #fff;
-  background: #009688;
-  filter: drop-shadow(0 0 0.25rem #00796b);
-}
-
-.fade-group-enter-active,
-.fade-group-leave-active,
-.fade-group-move {
-  transition: opacity 1s, transform 1s;
-}
-
-.fade-group-leave-active {
-  position: absolute;
-}
-
-.fade-group-enter {
-  opacity: 0;
-  transform: translateY(-1.25rem);
-}
-
-.fade-group-leave-to {
-  opacity: 0;
-  transform: translateY(1.25rem);
 }
 </style>
