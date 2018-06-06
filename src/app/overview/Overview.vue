8be5671811e86add1d7b5a37074c80a782ea08a3
Shyam Chen <shyamchen1994@gmail.com>
2018-06-06 20:29:14 +0800

    484th Commit

--- src/app/overview/Overview.vue
+++ src/app/overview/Overview.vue
@@ -11,10 +11,10 @@
         <div>04. Data Table - Paginator, Iterator</div>
         <div>05. Internationalization - Adapt applications and text to enable their usability in a particular cultural or linguistic market.</div>
         <div>06. Data Visualization - Produce dynamic, interactive data visualizations in applications.</div>
-        <div>07. Authorization - Email/Password, Google, Facebook, Twitter, Instagram, Line, Forgot Password</div>
+        <div>07. Authorization - Provide a user identity to applications.</div>
         <div>08. Payment - Process payments from users with Stripe.</div>
         <div>09. Messages - Send Email, Cloud Messaging, Chat Messages</div>
-        <div>10. Machine Learning - Label Images</div>
+        <div>10. Machine Learning - Improve user experience through artificial intelligence.</div>
       </v-layout>
     </v-layout>
   </v-container>
