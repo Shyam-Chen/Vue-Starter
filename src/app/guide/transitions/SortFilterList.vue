301ac7f7816685010e6d5bc67440132f77183c13
Shyam Chen <shyamchen1994@gmail.com>
2018-06-21 20:56:03 +0800

    504th Commit

--- src/app/guide/transitions/SortFilterList.vue
+++ src/app/guide/transitions/SortFilterList.vue
@@ -29,6 +29,9 @@
           <div class="card-content">
             <div class="pl-2 pr-2">{{ item.title | truncate(55) }}</div>
             <div class="ma-2"><v-icon>headset</v-icon> {{ item.views.toLocaleString('en-US') }}</div>
+            <div>
+              {{ item.publish | timeSince }}
+            </div>
           </div>
         </div>
       </template>
@@ -56,6 +59,47 @@ export default {
       if (second < 10) return `${minute}:0${second}`;
 
       return `${minute}:${second}`;
+    },
+    timeSince(date) {
+      const d = new Date(1529585376100);
+      const now = new Date();
+      console.log('[* 1]', date);
+
+      console.log('[* 2]', now.getTime());
+
+      const seconds = Math.round(Math.abs((now.getTime() - d.getTime()) / 1000));
+
+      const minutes = Math.round(Math.abs(seconds / 60));
+      const hours = Math.round(Math.abs(minutes / 60));
+      const days = Math.round(Math.abs(hours / 24));
+      const months = Math.round(Math.abs(days / 30.416));
+      const years = Math.round(Math.abs(days / 365));
+
+      if (Number.isNaN(seconds)) {
+        return '';
+      } else if (seconds <= 45) {
+        return 'a few seconds ago';
+      } else if (seconds <= 90) {
+        return 'a minute ago';
+      } else if (minutes <= 45) {
+        return `${minutes} minutes ago`;
+      } else if (minutes <= 90) {
+        return 'an hour ago';
+      } else if (hours <= 22) {
+        return `${hours} hours ago`;
+      } else if (hours <= 36) {
+        return 'a day ago';
+      } else if (days <= 25) {
+        return `${days} days ago`;
+      } else if (days <= 45) {
+        return 'a month ago';
+      } else if (days <= 345) {
+        return `${months} months ago`;
+      } else if (days <= 545) {
+        return 'a year ago';
+      }
+
+      return `${years} years ago`;
     },
   },
   data() {
