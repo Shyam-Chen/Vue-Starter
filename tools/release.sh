1630e910492389b59809e39a8284aba8c0bf93b1
Shyam Chen <shyamchen1994@gmail.com>
2018-06-28 17:28:17 +0800

    522nd Commit

--- tools/release.sh
+++ tools/release.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 set -e
+
+$ docker build -f tools/Dockerfile.<dev|prod> -t <IMAGE_NAME>:<IMAGE_TAG> .
+
+$ docker tag <IMAGE_NAME>:<IMAGE_TAG> <DOCKER_ID_USER>/<IMAGE_NAME>:<IMAGE_TAG>
+$ docker push <DOCKER_ID_USER>/<IMAGE_NAME>:<IMAGE_TAG>
