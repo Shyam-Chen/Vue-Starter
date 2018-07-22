0bcfd588a354eda1c9885dc95e4faf6294b5acf2
Shyam Chen <shyamchen1994@gmail.com>
2018-07-22 17:28:25 +0800

    563rd Commit

--- src/app/shell/overview/Overview.vue
+++ src/app/shell/overview/Overview.vue
@@ -5,15 +5,15 @@
 
       <v-layout column class="mt-1">
         <div>00. Hello World - A bare-bones Hello World example.</div>
-        <div>01. Guide - Structure Vue, Vue Router, and Vuex for applications.</div>
+        <div>01. Guide - Build modern web applications using the Vue stack.</div>
         <div>02. CRUD Operations - The four major functions implemented in applications.</div>
-        <div>03. Form Controls - Text Fields, Selects, Picker, File Upload, Markdown Editor, Autocomplete</div>
-        <div>04. Data Table - Paginator, Iterator</div>
+        <div>03. Form Controls - Get or collect information from users for applications.</div>
+        <div>04. Data Table - Process and display of data from applications.</div>
         <div>05. Internationalization - Adapt applications and text to enable their usability in a particular cultural or linguistic market.</div>
         <div>06. Data Visualization - Produce dynamic, interactive data visualizations in applications.</div>
         <div>07. Authorization - Provide a user identity to applications.</div>
-        <div>08. Payment - Process payments from users with Stripe.</div>
-        <div>09. Messages - Send Email, Cloud Messaging, Chat Messages</div>
+        <div>08. Payment - Process payments from users for applications.</div>
+        <div>09. Messages - Communicate or deliver messages to users for applications.</div>
         <div>10. Machine Learning - Improve user experience through artificial intelligence.</div>
       </v-layout>
     </v-layout>
