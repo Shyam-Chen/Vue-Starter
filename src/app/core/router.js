a7cc1b4dff7bcc45e49aca56a930582084aa3085
Shyam Chen <shyamchen1994@gmail.com>
2018-08-18 19:27:39 +0800

    579th Commit

--- src/app/core/router.js
+++ src/app/core/router.js
@@ -6,6 +6,8 @@ import Analytics from 'vue-analytics';
 import Home from '~/home/Home';
 import NotFound from '~/not-found/NotFound';
 
+import guide from '~/shell/guide/routes';
+
 Vue.use(Router);
 Vue.use(Meta);
 
@@ -22,45 +24,7 @@ const router = new Router({
     { path: '/hello-world', component: () => import('~/shell/hello-world/HelloWorld') },
 
     /** @name guide */
-    { path: '/guide', redirect: '/guide/template-rendering' },
-    { path: '/guide/template-rendering', component: () => import('~/shell/guide/template-rendering/TemplateRendering') },
-    { path: '/guide/components/props', component: () => import('~/shell/guide/components/props/Props') },
-    { path: '/guide/components/slots', component: () => import('~/shell/guide/components/slots/Slots') },
-    { path: '/guide/components/refs', component: () => import('~/shell/guide/components/refs/Refs') },
-    { path: '/guide/components/custom-events', component: () => import('~/shell/guide/components/custom-events/CustomEvents') },
-    { path: '/guide/components/dynamic', component: () => import('~/shell/guide/components/dynamic/Dynamic') },
-    { path: '/guide/lifecycle-hooks', component: () => import('~/shell/guide/lifecycle-hooks/LifecycleHooks') },
-    { path: '/guide/transitions', component: () => import('~/shell/guide/transitions/Transitions') },
-    { path: '/guide/mixins', component: () => import('~/shell/guide/mixins/Mixins') },
-    { path: '/guide/directives', component: () => import('~/shell/guide/directives/Directives') },
-    { path: '/guide/dependency-injection', component: () => import('~/shell/guide/dependency-injection/DependencyInjection') },
-    { path: '/guide/filters', component: () => import('~/shell/guide/filters/Filters') },
-    { path: '/guide/routing/dynamic-matching', component: () => import('~/shell/guide/routing/dynamic-matching/DynamicMatching') },
-    { path: '/guide/routing/dynamic-matching/:username', component: () => import('~/shell/guide/routing/dynamic-matching/UserInfo') },
-    {
-      path: '/guide/routing/nested-routes',
-      component: () => import('~/shell/guide/routing/nested-routes/NestedRoutes'),
-      children: [
-        { path: '', component: () => import('~/shell/guide/routing/nested-routes/Default') },
-        { path: 'first', component: () => import('~/shell/guide/routing/nested-routes/First') },
-        { path: 'second', component: () => import('~/shell/guide/routing/nested-routes/Second') },
-      ],
-    },
-    {
-      path: '/guide/routing/guards',
-      component: () => import('~/shell/guide/routing/guards/Guards'),
-      beforeEnter(to, from, next) {
-        next();
-      },
-    },
-    {
-      path: '/guide/routing/guards/:id',
-      component: () => import('~/shell/guide/routing/guards/Guards'),
-      beforeEnter(to, from, next) {
-        next();
-      },
-    },
-    { path: '/guide/state-management', component: () => import('~/shell/guide/state-management/StateManagement') },
+    ...guide,
 
     /** @name crud-operations */
     { path: '/crud-operations', redirect: '/crud-operations/basic' },
