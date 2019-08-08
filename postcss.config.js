56019a26a0a77482017d722f2258777e45e56b30
Shyam Chen <shyamchen1994@gmail.com>
2019-08-08 10:37:19 +0800

    706th Commit

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     'postcss-preset-env': {
       stage: 0,
-      browsers: 'last 2 versions',
+      browserslist: 'last 2 versions',
     },
     'postcss-nested': {},
     cssnano: {},
