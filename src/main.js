1fafaee013364f0b679bc88ec33b413d32aae984
Shyam Chen <shyamchen1994@gmail.com>
2019-09-08 19:12:33 +0800

    716th Commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import { sync } from 'vuex-router-sync';
 import Raven from 'raven-js';
 import RavenVue from 'raven-js/plugins/vue';
-import { register } from 'register-service-worker';
 
 import '~/assets/styles/global.css';
 import App from '~/App';
@@ -11,6 +10,7 @@ import store from '~/core/store';
 import vuetify from '~/core/vuetify';
 import apolloProvider from '~/core/apollo-provider';
 import i18n from '~/core/i18n';
+import registerServiceWorker from '~/core/service-worker';
 
 sync(store, router);
 
@@ -18,32 +18,6 @@ if (process.env.NODE_ENV === 'production') {
   Raven.config(process.env.SENTRY_DSN)
     .addPlugin(RavenVue, Vue)
     .install();
-
-  register('/service-worker.js', {
-    registrationOptions: { scope: './' },
-    ready() {
-      console.log('Service worker is active.');
-    },
-    registered() {
-      console.log('Service worker has been registered.');
-    },
-    cached() {
-      console.log('Content has been cached for offline use.');
-    },
-    updatefound() {
-      console.log('New content is downloading.');
-    },
-    updated() {
-      console.log('New content is available; please refresh.');
-      window.location.reload();
-    },
-    offline() {
-      console.log('No internet connection found. App is running in offline mode.');
-    },
-    error(error) {
-      console.error('Error during service worker registration:', error);
-    },
-  });
 }
 
 const vm = new Vue({
@@ -56,6 +30,8 @@ const vm = new Vue({
 });
 
 vm.$mount('#app');
+
+registerServiceWorker();
 
 if (module.hot) {
   module.hot.accept();
