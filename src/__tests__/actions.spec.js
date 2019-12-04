ae3b5b11d84a2a9f121ccee695dbb8d44c5d2096
Shyam Chen <shyamchen1994@gmail.com>
2019-12-04 11:53:26 +0800

    746th Commit

--- src/__tests__/actions.spec.js
+++ src/__tests__/actions.spec.js
@@ -2,33 +2,41 @@ import { INITIAL } from '../constants';
 import actions from '../actions';
 
 describe('actions', () => {
-  // it('should handle setTheme', () => {
-  //   const state = { ...INITIAL };
-  //   const payload = 'dark';
+  it('should handle setTheme', () => {
+    const state = { ...INITIAL };
+    const payload = 'dark';
+    actions.setTheme({ state }, payload);
+    expect(localStorage.getItem('theme')).toEqual('dark');
+  });
 
-  //   expect(state.theme).toMatch('light');
+  it('should handle setLanguage', () => {
+    const state = {
+      ...INITIAL,
+      route: {
+        path: '/ja/overview',
+        params: { lang: 'ja' },
+      },
+    };
 
-  //   actions.setTheme({ state }, payload);
+    const payload = 'ja';
 
-  //   expect(state.theme).toMatch('dark');
-  // });
+    actions.setLanguage({ state }, payload);
+  });
 
-  // it('should handle setLanguage and initialLanguage', () => {
-  //   Object.defineProperty(window.navigator, 'language', { value: 'zh' });
-  //   sessionStorage.setItem('lang', 'zh');
+  it('should handle initialLanguage', () => {
+    const context = {
+      state: {
+        ...INITIAL,
+        route: { params: { lang: 'ja' } },
+      },
+      dispatch(type, val) {
+        expect(type).toBe('setLanguage');
+        expect(val).toBe('ja');
+      },
+    };
 
-  //   const context = {};
-  //   const payload = 'zh';
-
-  //   actions.setLanguage(context, payload);
-
-  //   const dispatch = (type, val) => {
-  //     expect(type).toBe('setLanguage');
-  //     expect(val).toBe('zh');
-  //   };
-
-  //   actions.initialLanguage({ dispatch });
-  // });
+    actions.initialLanguage(context);
+  });
 
   it('should handle backToHome', () => {
     const state = { ...INITIAL };
--- tools/setup-app.js
+++ tools/setup-app.js
@@ -1,13 +1,15 @@
-import 'jest-localstorage-mock';
-import { toMatchImageSnapshot } from 'jest-image-snapshot';
 import Vue from 'vue';
 import Vuetify from 'vuetify';
-
-expect.extend({ toMatchImageSnapshot });
+import { JSDOM } from 'jsdom';
 
 Vue.config.productionTip = false;
 
 Vue.use(Vuetify);
+
+const jsdom = new JSDOM('<!DOCTYPE html><html><head></head><body></body></html>');
+
+global.window = jsdom.window;
+global.document = jsdom.window.document;
 
 window.matchMedia = () => ({ matches: false, addListener() {}, removeListener() {} });
 
