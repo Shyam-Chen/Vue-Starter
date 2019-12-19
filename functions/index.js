8d0f68cba4ea82578e10cbe90612186165b62ebc
Shyam-Chen <shyamchen1994@gmail.com>
2019-12-19 17:46:22 +0800

    769th Commit

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-const functions = require('firebase-functions');
-const express = require('express');
-const prerender = require('prerender-node');
-
-const app = express();
-
-app.use(prerender.set('prerenderToken', 'QYKyxDcMkqVVLNn7AREr'));
-
-exports.app = functions.https.onRequest(app);
