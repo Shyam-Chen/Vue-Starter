b2b4787532813e7184d04a14ac2d30fd7fd21cd0
Shyam-Chen <shyamchen1994@gmail.com>
2022-07-29 14:36:14 +0800

    869th Commit

--- src/modules/store-pattern/Counter.vue
+++ src/modules/store-pattern/Counter.vue
@@ -1,13 +1,15 @@
 <script setup>
-import { useState, useActions, useComputeds } from './store.js';
+import { useState, useActions, useComputeds, useReactions } from './store.js';
 
 const state = useState();
 const actions = useActions();
 const computeds = useComputeds();
+const reactions = useReactions();
 </script>
 
 <template>
   <div>Clicked: {{ state.count }} times, value is {{ computeds.evenOrOdd.value }}.</div>
+  <dir>{{ state.prevCount }}</dir>
 
   <button
     class="bg-blue-500 hover:bg-blue-700 text-white px-4 py-1 rounded"
--- src/modules/store-pattern/store.js
+++ src/modules/store-pattern/store.js
@@ -1,9 +1,10 @@
-import { provide, inject, reactive, computed } from 'vue';
+import { computed, reactive, watch, provide, inject } from 'vue';
 
 const stateSymbol = Symbol('counter');
 
 const createState = reactive({
   count: 0,
+  prevCount: null,
 });
 
 export const useState = () => inject(stateSymbol);
@@ -11,22 +12,43 @@ export const useState = () => inject(stateSymbol);
 export const useActions = () => {
   const state = useState();
 
-  return {
+  const actions = {
     increment() {
       state.count += 1;
     },
     decrement() {
       state.count -= 1;
     },
+    setCount(val) {
+      state.count = val;
+    },
+    resetCount() {
+      actions.setCount(0);
+    },
   };
+
+  return actions;
 };
 
 export const useComputeds = () => {
   const state = useState();
 
-  return {
+  const computeds = {
     evenOrOdd: computed(() => (state.count % 2 === 0 ? 'Even' : 'Odd')),
   };
+
+  return computeds;
+};
+
+export const useReactions = () => {
+  const state = useState();
+
+  watch(
+    () => state.count,
+    (val, oldVal) => {
+      state.prevCount = oldVal;
+    },
+  );
 };
 
 export default () => provide(stateSymbol, createState);
