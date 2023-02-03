4759304b3a779404990e28bdaca37bae0c48137b
Shyam-Chen <shyamchen1994@gmail.com>
2023-02-03 10:40:11 +0800

    940th Commit

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -2,44 +2,62 @@
 import { computed } from 'vue';
 import uniqueId from 'lodash/uniqueId';
 
-const props = defineProps({
-  value: {
-    type: Boolean,
-    default: false,
-  },
-});
+const props = defineProps<{
+  value?: boolean;
+  checked?: boolean;
+  disabled?: boolean;
+  indeterminate?: boolean;
+}>();
 
-const emit = defineEmits(['update:value']);
+const emit = defineEmits<{
+  (evt: 'update:value', val: boolean): void;
+}>();
 
 const uid = uniqueId('checkbox-');
 
 const checkboxValue = computed({
-  get: () => props.value,
+  get: () => props.value || false,
   set: (val) => emit('update:value', val),
 });
 </script>
 
 <template>
-  <div class="checkbox flex items-center">
-    <input
-      :id="uid"
-      v-model="checkboxValue"
-      v-bind="$attrs"
-      type="checkbox"
-      class="accent-blue-600 border-gray-300 rounded h-4 w-4"
-      :class="[$attrs.hasOwnProperty('disabled') ? 'cursor-not-allowed' : 'cursor-pointer']"
-    />
+  <label
+    class="flex items-center"
+    :class="[disabled ? 'cursor-not-allowed opacity-60' : checked ? '' : 'cursor-pointer']"
+  >
+    <div class="relative flex justify-center items-center">
+      <input
+        :id="uid"
+        v-model="checkboxValue"
+        v-bind="$attrs"
+        :disabled="disabled"
+        type="checkbox"
+        class="checkbox"
+        :class="[
+          {
+            'bg-primary-500': value || checked || indeterminate,
+            'bg-white': !(value || checked || indeterminate),
+          },
+          disabled ? 'cursor-not-allowed' : checked ? '' : 'cursor-pointer',
+        ]"
+      />
 
-    <label :for="uid" class="checkbox-label ml-2">
-      <slot />
-    </label>
-  </div>
+      <div
+        class="absolute select-none w-4 h-4 text-white"
+        :class="[indeterminate ? 'i-mdi-minus-thick' : value || checked ? 'i-mdi-check-bold' : '']"
+      ></div>
+    </div>
+
+    <div class="ml-2 empty:hidden">
+      <slot></slot>
+    </div>
+  </label>
 </template>
 
 <style lang="scss" scoped>
 .checkbox {
-  &-label:empty {
-    display: none;
-  }
+  @apply appearance-none w-5 h-5 rounded border border-slate-400 dark:border-slate-600;
+  @apply focus:outline-none focus:ring-2 focus:ring-primary-400 focus:shadow-lg;
 }
 </style>
--- src/components/CheckboxGroup.vue
+++ src/components/CheckboxGroup.vue
@@ -1,25 +1,22 @@
 <script lang="ts" setup>
-import type { PropType } from 'vue';
 import { computed } from 'vue';
 import uniqueId from 'lodash/uniqueId';
 
-const props = defineProps({
-  value: {
-    type: Array as PropType<any[]>,
-    default: () => [],
-  },
-  options: {
-    type: Array as PropType<any[]>,
-    default: () => [],
-  },
-});
+const props = defineProps<{
+  value?: string[];
+  options?: string[] | Array<{ label: string; value: string }>;
+  disabled?: boolean;
+}>();
 
-const emit = defineEmits(['update:value', 'change']);
+const emit = defineEmits<{
+  (evt: 'update:value', val: string[]): void;
+  (evt: 'change', val: string[]): void;
+}>();
 
 const uid = uniqueId('checkbox-group-');
 
 const checkboxGroupValue = computed({
-  get: () => props.value,
+  get: () => props.value || [],
   set: (val) => emit('update:value', val),
 });
 </script>
@@ -27,20 +24,51 @@ const checkboxGroupValue = computed({
 <template>
   <div class="flex items-center space-x-4">
     <div v-for="(item, idx) in options" :key="idx" class="flex items-center">
-      <input
-        :id="`${uid}-${idx}`"
-        v-model="checkboxGroupValue"
-        v-bind="$attrs"
-        type="checkbox"
-        :name="uid"
-        :value="typeof item === 'object' ? item.value : item"
-        class="accent-blue-600 border-gray-300 rounded h-4 w-4 cursor-pointer"
-        @change="emit('change', checkboxGroupValue)"
-      />
+      <label
+        class="flex items-center"
+        :class="[disabled ? 'cursor-not-allowed opacity-60' : 'cursor-pointer']"
+      >
+        <div class="relative flex justify-center items-center">
+          <input
+            :id="`${uid}-${idx}`"
+            v-model="checkboxGroupValue"
+            v-bind="$attrs"
+            type="checkbox"
+            :name="uid"
+            :value="typeof item === 'object' ? item.value : item"
+            class="checkbox"
+            :class="{
+              'bg-primary-500': checkboxGroupValue.includes(
+                typeof item === 'object' ? item.value : item,
+              ),
+              'bg-white': !checkboxGroupValue.includes(
+                typeof item === 'object' ? item.value : item,
+              ),
+            }"
+            @change="emit('change', checkboxGroupValue)"
+          />
 
-      <label :for="`${uid}-${idx}`" class="ml-2 cursor-pointer">
-        {{ typeof item === 'object' ? item.label : item }}
+          <div
+            class="absolute select-none w-4 h-4 text-white"
+            :class="{
+              'i-mdi-check-bold': checkboxGroupValue.includes(
+                typeof item === 'object' ? item.value : item,
+              ),
+            }"
+          ></div>
+        </div>
+
+        <div class="ml-2">
+          {{ typeof item === 'object' ? item.label : item }}
+        </div>
       </label>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.checkbox {
+  @apply appearance-none w-5 h-5 rounded border border-slate-400 dark:border-slate-600;
+  @apply focus:outline-none focus:ring-2 focus:ring-primary-400 focus:shadow-lg;
+}
+</style>
