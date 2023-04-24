70397927f938ee7c25ef6352548d631343bccf48
Shyam-Chen <shyamchen1994@gmail.com>
2023-04-24 10:42:38 +0800

    980th Commit

--- src/components/Transfer.vue
+++ src/components/Transfer.vue
@@ -1,68 +1,160 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, computed, watch, toRef } from 'vue';
 import Draggable from 'vuedraggable';
+import remove from 'lodash/remove';
 
 import Checkbox from './Checkbox.vue';
 import Button from './Button.vue';
 
-defineProps<{
-  value?: any[];
-  data?: any[];
+type Item = {
+  id?: string | number;
+  name?: string;
+  checked?: boolean;
+};
+
+const props = withDefaults(
+  defineProps<{
+    source?: Item[];
+    target?: Item[];
+  }>(),
+  {
+    source: () => [],
+    target: () => [],
+  },
+);
+
+const emit = defineEmits<{
+  (evt: 'update:source', val?: Item[]): void;
+  (evt: 'update:target', val?: Item[]): void;
 }>();
 
-const list1 = ref([
-  { name: 'John', id: 1 },
-  { name: 'Joao', id: 2 },
-  { name: 'Jean', id: 3 },
-  { name: 'Gerard', id: 4 },
-  { name: 'Angular', id: 8 },
-  { name: 'React', id: 9 },
-  { name: 'Svelte', id: 10 },
-  { name: 'Vue', id: 11 },
-  { name: 'TypeScript', id: 12 },
-  { name: 'JavaScript', id: 13 },
-  { name: 'CoffeeScript', id: 14 },
-  { name: 'PureScript', id: 15 },
-]);
+const sourceList = ref(false);
+const sourceListIndeterminate = ref(false);
+const list1 = toRef(props, 'source');
+const checkedSourceList = computed(() => list1.value.filter((item) => item.checked).length);
 
-const list2 = ref([
-  { name: 'Juan', id: 5 },
-  { name: 'Edgard', id: 6 },
-  { name: 'Johnson', id: 7 },
-]);
+const targetList = ref(false);
+const targetListIndeterminate = ref(false);
+const list2 = toRef(props, 'target');
+const checkedTargetList = computed(() => list2.value.filter((item) => item.checked).length);
+
+function changeList() {
+  const arr1 = [...list1.value].map((item) => ({ ...item, checked: false }));
+  const arr2 = [...list2.value].map((item) => ({ ...item, checked: false }));
+  emit('update:source', arr1);
+  emit('update:target', arr2);
+}
+
+watch(
+  () => sourceList.value,
+  (checked) => {
+    const arr = [...list1.value].map((item) => ({ ...item, checked }));
+    emit('update:source', arr);
+  },
+);
+
+watch(
+  () => list1.value,
+  (val) => {
+    const checked = val.every((item) => item.checked);
+    const unchecked = val.every((item) => !item.checked);
+
+    sourceListIndeterminate.value = !(checked || unchecked);
+
+    if (checked) sourceList.value = true;
+    if (unchecked) sourceList.value = false;
+  },
+  { deep: true },
+);
+
+watch(
+  () => targetList.value,
+  (checked) => {
+    const arr = [...list2.value].map((item) => ({ ...item, checked }));
+    emit('update:target', arr);
+  },
+);
+
+watch(
+  () => list2.value,
+  (val) => {
+    const checked = val.every((item) => item.checked);
+    const unchecked = val.every((item) => !item.checked);
+
+    targetListIndeterminate.value = !(checked || unchecked);
+
+    if (checked) targetList.value = true;
+    if (unchecked) targetList.value = false;
+  },
+  { deep: true },
+);
+
+function toRight() {
+  const arr = remove(list1.value, (item) => item.checked);
+  const arr2 = [...list2.value, ...arr].map((item) => ({ ...item, checked: false }));
+  emit('update:target', arr2);
+}
+
+function toLeft() {
+  const arr = remove(list2.value, (item) => item.checked);
+  const arr1 = [...list1.value, ...arr].map((item) => ({ ...item, checked: false }));
+  emit('update:source', arr1);
+}
 </script>
 
 <template>
   <div class="transfer">
     <div class="container">
       <div class="title">
-        <Checkbox>Source List</Checkbox>
-        <span class="text-sm text-slate-400">0/12</span>
+        <Checkbox v-model:value="sourceList" :indeterminate="sourceListIndeterminate">
+          Source List
+        </Checkbox>
+
+        <span class="text-sm text-slate-400">{{ checkedSourceList }}/{{ list1.length }}</span>
       </div>
 
-      <Draggable :list="list1" itemKey="name" class="list">
-        <template #item="{ element, index }">
-          <div>
-            <Checkbox v-model:value="element.checked">{{ element.name }} {{ index }}</Checkbox>
+      <Draggable
+        :list="list1"
+        group="list"
+        itemKey="name"
+        class="list"
+        style="height: calc(100% - 54px)"
+        @change="changeList"
+      >
+        <template #item="{ element }">
+          <div class="item">
+            <Checkbox v-model:value="element.checked">{{ element.name }} </Checkbox>
           </div>
         </template>
       </Draggable>
     </div>
 
     <div class="controller">
-      <Button icon="i-mdi-chevron-left" />
-      <Button icon="i-mdi-chevron-right" />
+      <Button icon="i-mdi-chevron-right" @click="toRight" />
+      <Button icon="i-mdi-chevron-left" @click="toLeft" />
     </div>
 
     <div class="container">
       <div class="title">
-        <Checkbox>Target List</Checkbox>
-        <span class="text-sm text-slate-400">0/3</span>
+        <Checkbox v-model:value="targetList" :indeterminate="targetListIndeterminate">
+          Target List
+        </Checkbox>
+
+        <span class="text-sm text-slate-400">{{ checkedTargetList }}/{{ list2.length }}</span>
       </div>
 
-      <Draggable :list="list2" itemKey="name" class="list">
-        <template #item="{ element, index }">
-          <Checkbox v-model:value="element.checked">{{ element.name }} {{ index }}</Checkbox>
+      <Draggable
+        :list="list2"
+        group="list"
+        itemKey="name"
+        class="list"
+        style="height: calc(100% - 54px)"
+        @change="changeList"
+      >
+        <template #item="{ element }">
+          <div class="item">
+            <Checkbox v-model:value="element.checked">{{ element.name }}</Checkbox>
+          </div>
         </template>
       </Draggable>
     </div>
@@ -84,7 +176,11 @@ const list2 = ref([
 }
 
 .list {
-  @apply flex flex-col gap-2 px-3 py-2;
+  @apply flex flex-col gap-1 bg-slate-900;
+}
+
+.item {
+  @apply bg-slate-800 px-3 py-1;
 }
 
 .controller {
--- src/modules/(library)/data-entry/transfer/Registry.vue
+++ src/modules/(library)/data-entry/transfer/Registry.vue
@@ -1,6 +1,23 @@
 <script lang="ts" setup>
+import { reactive } from 'vue';
+
 import Breadcrumbs from '~/components/Breadcrumbs.vue';
 import Transfer from '~/components/Transfer.vue';
+
+const flux = reactive({
+  source: [
+    { id: 1, name: 'Angular' },
+    { id: 2, name: 'React' },
+    { id: 3, name: 'Svelte' },
+    { id: 4, name: 'Vue' },
+  ],
+  target: [
+    { id: 5, name: 'TypeScript' },
+    { id: 6, name: 'JavaScript' },
+    { id: 7, name: 'CoffeeScript' },
+    { id: 8, name: 'PureScript' },
+  ],
+});
 </script>
 
 <template>
@@ -20,8 +37,14 @@ import Transfer from '~/components/Transfer.vue';
   <div class="flex flex-col border p-4 mb-4">
     <div class="mb-2">Basic</div>
 
-    <div class="flex justify-center">
-      <Transfer />
+    <div class="flex flex-col justify-center">
+      <Transfer v-model:source="flux.source" v-model:target="flux.target" />
+    </div>
+
+    <div class="flex">
+      <pre class="flex-1">{{ flux.source }}</pre>
+      <div class="w-15"></div>
+      <pre class="flex-1">{{ flux.target }}</pre>
     </div>
   </div>
 </template>
