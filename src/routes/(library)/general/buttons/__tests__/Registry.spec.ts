3d74eea388031eaf3528d89a99df40ca2b7f32fa
Shyam-Chen <shyamchen1994@gmail.com>
2023-05-22 09:51:39 +0800

    993rd Commit

--- src/routes/(library)/general/buttons/__tests__/Registry.spec.ts
+++ src/routes/(library)/general/buttons/__tests__/Registry.spec.ts
@@ -1,11 +1,21 @@
-import { test, expect } from 'vitest';
+import { test, expect, vi } from 'vitest';
 import { render } from '@testing-library/vue';
 
 import router from '~/plugins/router';
+import localer from '~/plugins/localer';
 
 import Registry from '../Registry.vue';
 
 test('Buttons', () => {
-  const { html } = render(Registry, { global: { plugins: [router] } });
+  vi.mock('~/utilities/request', () => {
+    return {
+      default: vi.fn(() => () => ({
+        _data: {},
+        status: 200,
+      })),
+    };
+  });
+
+  const { html } = render(Registry, { global: { plugins: [router, localer] } });
   expect(html()).toMatchSnapshot();
 });
