e7df596e5e499786589ef6bac8192114a35aab53
Shyam-Chen <shyamchen1994@gmail.com>
2023-05-22 10:45:41 +0800

    994th Commit

--- benchmark/src/entry.spec.ts
+++ benchmark/src/entry.spec.ts
@@ -2,17 +2,17 @@ import { test, expect } from 'vitest';
 import { chromium } from 'playwright';
 import lighthouse from 'lighthouse';
 
-test('Sign-in', async () => {
+test('Entry', async () => {
   const browser = await chromium.launch({ args: ['--remote-debugging-port=9222'] });
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto('http://localhost:8080/sign-in');
+  await page.goto('http://localhost:8080');
   await page.waitForLoadState('networkidle');
 
-  const { lhr } = await lighthouse('http://localhost:8080/sign-in');
-  const performance = lhr.categories.performance.score * 100;
+  const runnerResult = await lighthouse('http://localhost:8080');
 
-  expect(performance).toBe(100);
+  const performance = Number(runnerResult?.lhr.categories.performance.score) * 100;
+  expect(performance).toBe(90);
 
   await browser.close();
 });
