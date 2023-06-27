1637785eb14cc2f40a0137787199228fad2bdd90
Shyam-Chen <shyamchen1994@gmail.com>
2023-06-27 12:08:33 +0800

    1,017th Commit

--- benchmark/src/entry.spec.ts
+++ benchmark/src/entry.spec.ts
@@ -1,18 +1,26 @@
-import { test, expect } from 'vitest';
+import type { Browser, Page } from 'playwright';
+import { beforeAll, afterAll, test, expect } from 'vitest';
 import { chromium } from 'playwright';
 import lighthouse from 'lighthouse';
 
-test('Entry', async () => {
-  const browser = await chromium.launch({ args: ['--remote-debugging-port=9222'] });
-  const context = await browser.newContext();
-  const page = await context.newPage();
+let browser: Browser;
+let page: Page;
+
+beforeAll(async () => {
+  browser = await chromium.launch({ args: ['--remote-debugging-port=9222'] });
+  page = await browser.newPage();
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test('entry point', async () => {
   await page.goto('http://localhost:8080');
   await page.waitForLoadState('networkidle');
 
   const runnerResult = await lighthouse('http://localhost:8080');
 
-  const performance = Number(runnerResult?.lhr.categories.performance.score) * 100;
-  expect(performance).toBe(90);
-
-  await browser.close();
+  const performanceScore = Number(runnerResult?.lhr.categories.performance.score) * 100;
+  expect(performanceScore).toBe(90);
 });
