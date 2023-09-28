5775dfb691902c78542341e97dbb5913586496b8
Shyam-Chen <shyamchen1994@gmail.com>
2023-09-28 10:34:51 +0800

    1,103rd Commit

--- src/Error.vue
+++ src/Error.vue
@@ -1,26 +1,63 @@
 <script lang="ts" setup>
-import { RouterLink } from 'vue-router';
+import { reactive, onBeforeMount } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+
+import Button from '~/components/Button.vue';
+
+const router = useRouter();
+const route = useRoute();
+
+const message = reactive({
+  statusCode: '404',
+  title: 'Not Found',
+  description: `Sorry, we couldn't find this page.`,
+});
+
+const backToHomepage = () => {
+  router.replace('/');
+};
+
+onBeforeMount(() => {
+  if (route.query.code === '403') {
+    message.statusCode = '403';
+    message.title = 'Access Denied';
+    message.description = `Sorry, but you don't have permission to access this page.`;
+  }
+
+  if (route.query.code === '500') {
+    message.statusCode = '500';
+    message.title = 'Internal Server Error';
+    message.description = `Sorry, we are already working to solve the problem.`;
+  }
+});
 </script>
 
 <template>
-  <div class="flex justify-center items-center min-h-screen h-full p-16">
-    <div class="container flex flex-col items-center justify-center px-5 mx-auto my-8">
-      <div class="max-w-md text-center">
-        <h2 class="mb-8 font-extrabold text-9xl text-gray-400">
-          <span class="sr-only">Error</span>404
-        </h2>
-        <p class="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
-        <p class="mt-4 mb-8">
-          But dont worry, you can find plenty of other things on our homepage.
-        </p>
-        <RouterLink
-          rel="noopener noreferrer"
-          to="/"
-          class="px-8 py-3 font-semibold rounded bg-blue-600 text-gray-50"
-        >
-          Back to homepage
-        </RouterLink>
+  <div class="flex justify-center items-center min-h-screen h-full p-8 md:p-16">
+    <div class="container flex flex-col items-center justify-center px-5 mx-auto">
+      <div class="mx-auto max-w-screen-sm text-center">
+        <div class="Error-StatusCode">{{ message.statusCode }}</div>
+        <div class="Error-Title">{{ message.title }}</div>
+        <div class="Error-Description">{{ message.description }}</div>
+
+        <div class="flex justify-center">
+          <Button @click="backToHomepage">Back to Homepage</Button>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.Error-StatusCode {
+  @apply mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500;
+}
+
+.Error-Title {
+  @apply mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white;
+}
+
+.Error-Description {
+  @apply mb-4 text-lg font-light text-gray-500 dark:text-gray-400;
+}
+</style>
