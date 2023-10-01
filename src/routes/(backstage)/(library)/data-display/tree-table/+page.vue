2beeb91df7516dad9fcdea732a2edb470a31519e
Shyam-Chen <shyamchen1994@gmail.com>
2023-10-01 20:57:45 +0800

    1,107th Commit

--- src/routes/(backstage)/(library)/data-display/tree-table/+page.vue
+++ src/routes/(backstage)/(library)/data-display/tree-table/+page.vue
@@ -4,35 +4,76 @@ import { ref } from 'vue';
 import Breadcrumbs from '~/components/Breadcrumbs.vue';
 import Button from '~/components/Button.vue/Button.vue';
 
-const treeTableNodes = ref([
+type TreeTableNode = {
+  key?: string;
+  name?: string;
+  size?: string;
+  type?: string;
+  level?: number;
+  status?: boolean;
+  children?: TreeTableNode[];
+};
+
+const initial = ref<TreeTableNode[]>([
   {
+    key: '1',
     name: 'Applications',
     size: '100kb',
     type: 'Folder',
+    level: 1,
     children: [
       {
+        key: '1-1',
         name: 'Vue',
         size: '25kb',
         type: 'Folder',
+        level: 2,
         children: [
-          { name: 'vue.app', size: '10kb', type: 'Application' },
-          { name: 'native.app', size: '10kb', type: 'Application' },
+          { key: '1-1-1', name: 'vue.app', size: '10kb', type: 'Application', level: 3 },
+          { key: '1-1-3', name: 'native.app', size: '10kb', type: 'Application', level: 3 },
         ],
       },
-      { name: 'Svelte', size: '25kb', type: 'Folder' },
+      { key: '1-2', name: 'Svelte', size: '25kb', type: 'Folder', level: 2 },
     ],
   },
   {
+    key: '2',
     name: 'Cloud',
     size: '500kb',
     type: 'Folder',
+    level: 1,
     children: [
-      { name: 'AWS', size: '25kb', type: 'Folder' },
-      { name: 'Azure', size: '25kb', type: 'Folder' },
-      { name: 'GCP', size: '25kb', type: 'Folder' },
+      { key: '2-1', name: 'AWS', size: '25kb', type: 'Folder', level: 2 },
+      { key: '2-2', name: 'Azure', size: '25kb', type: 'Folder', level: 2 },
+      { key: '2-3', name: 'GCP', size: '25kb', type: 'Folder', level: 2 },
     ],
   },
+  {
+    key: '3',
+    name: 'Desktop',
+    size: '750kb',
+    type: 'Folder',
+    level: 1,
+  },
 ]);
+
+const treeTableNodes = ref([...initial.value]);
+
+function treeNode(item: TreeTableNode) {
+  item.status = !item.status;
+
+  if (item.status) {
+    const arr = [...treeTableNodes.value];
+    const index = treeTableNodes.value.findIndex((node) => item.key === node.key);
+    arr.splice(index + 1, 0, ...(item?.children || []));
+    treeTableNodes.value = arr;
+  } else {
+    const arr = [...treeTableNodes.value];
+    const index = treeTableNodes.value.findIndex((node) => item.key === node.key);
+    arr.splice(index + 1, item?.children?.length);
+    treeTableNodes.value = arr;
+  }
+}
 </script>
 
 <template>
@@ -46,25 +87,48 @@ const treeTableNodes = ref([
     <table class="TreeTable-Element">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Size</th>
-          <th>Type</th>
+          <th
+            class="px-6 bg-blueGray-200 dark:bg-blueGray-700 text-blueGray-500 dark:text-blueGray-200 align-middle border border-solid border-blueGray-100 dark:border-blueGray-600 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
+          >
+            Name
+          </th>
+          <th
+            class="px-6 bg-blueGray-200 dark:bg-blueGray-700 text-blueGray-500 dark:text-blueGray-200 align-middle border border-solid border-blueGray-100 dark:border-blueGray-600 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
+          >
+            Size
+          </th>
+          <th
+            class="px-6 bg-blueGray-200 dark:bg-blueGray-700 text-blueGray-500 dark:text-blueGray-200 align-middle border border-solid border-blueGray-100 dark:border-blueGray-600 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
+          >
+            Type
+          </th>
         </tr>
       </thead>
 
       <tbody>
-        <tr v-for="node in treeTableNodes" :key="node.name">
-          <td>
-            <div class="flex">
-              <Button icon="i-fa-caret-down" color="secondary" variant="text" />
-              <div>{{ node.name }}</div>
-              <div v-if="node.children?.length">
-                <pre>{{ node.children }}</pre>
-              </div>
+        <tr
+          v-for="node in treeTableNodes"
+          :key="node.name"
+          class="hover:bg-slate-100 dark:hover:bg-slate-600 border-b last:border-b-0 dark:border-slate-600"
+        >
+          <td class="px-6 py-3 align-middle whitespace-nowrap">
+            <div
+              class="flex items-center"
+              :style="{ 'padding-left': Number(node.level) > 1 ? `${node.level}rem` : '0' }"
+            >
+              <Button
+                v-if="node.children?.length"
+                icon="i-fa-caret-down"
+                color="secondary"
+                variant="text"
+                @click="treeNode(node)"
+              />
+              <div v-else class="w-38px h-38px"></div>
+              <div class="ml-2">{{ node.name }} - {{ node.level }}</div>
             </div>
           </td>
-          <td>{{ node.size }}</td>
-          <td>{{ node.type }}</td>
+          <td class="px-6 py-3 align-middle whitespace-nowrap">{{ node.size }}</td>
+          <td class="px-6 py-3 align-middle whitespace-nowrap">{{ node.type }}</td>
         </tr>
       </tbody>
     </table>
