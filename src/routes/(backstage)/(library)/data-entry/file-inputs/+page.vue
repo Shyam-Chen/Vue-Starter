e6dc6ef07163dd2e82e3ce11dfefb48effcf1089
Shyam-Chen <shyamchen1994@gmail.com>
2023-10-19 08:13:05 +0800

    1,133rd Commit

--- src/routes/(backstage)/(library)/data-entry/file-inputs/+page.vue
+++ src/routes/(backstage)/(library)/data-entry/file-inputs/+page.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { reactive } from 'vue';
+import { ref, reactive } from 'vue';
 
 import Breadcrumbs from '~/components/Breadcrumbs.vue';
 import FileInput from '~/components/FileInput.vue';
@@ -34,12 +34,26 @@ const flux = reactive({
   },
 });
 
-function onDrop(evt: Event) {
-  console.log(evt);
+const fileList = ref<string[]>([]);
+const dragover = ref(false);
+
+function onDrop(evt: DragEvent) {
+  const files = Array.from(evt.dataTransfer?.files || []);
+  fileList.value = files.map((file) => file.name);
 }
 
-function onDragover(evt: Event) {
-  console.log(evt);
+function onChange(evt: Event) {
+  const el = evt.target as HTMLInputElement;
+  const files = Array.from(el.files || []);
+  fileList.value = files.map((file) => file.name);
+}
+
+function onDragOver() {
+  dragover.value = true;
+}
+
+function onDragLeave() {
+  dragover.value = false;
 }
 </script>
 
@@ -92,15 +106,35 @@ function onDragover(evt: Event) {
   </div>
 
   <div class="flex flex-col border p-4 mb-4">
-    <div class="mb-2">Drag Zone (TODO)</div>
+    <div class="mb-2">Drag Zone</div>
 
-    <div
-      class="border-4 border-dashed border-primary-600 rounded bg-primary-200 h-64 flex flex-col justify-center items-center"
+    <label
+      class="Dropzone-Element"
+      :class="{ 'important:bg-primary-200': dragover }"
+      @dragover.prevent="onDragOver"
+      @dragleave.prevent="onDragLeave"
       @drop.prevent="onDrop"
-      @dragover.prevent="onDragover"
     >
+      <input
+        ref="dropzone"
+        type="file"
+        class="hidden"
+        @change="onChange"
+        @click="($refs.dropzone as HTMLInputElement).value = ''"
+      />
       <div class="i-mdi-tray-arrow-down w-24 h-24 text-primary-500"></div>
       <div class="text-primary-700 text-2xl">Choose a file or drag it here.</div>
+    </label>
+
+    <div v-for="file in fileList" :key="file">
+      {{ file }}
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.Dropzone-Element {
+  @apply w-full h-64 flex flex-col justify-center items-center cursor-pointer;
+  @apply border-4 border-dashed border-primary-600 rounded bg-primary-200/75 hover:bg-primary-200;
+}
+</style>
