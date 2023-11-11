e5bb269c34f251f7615326d130b9c309f3918ce9
Shyam-Chen <shyamchen1994@gmail.com>
2023-11-11 16:44:49 +0800

    1,199th Commit

---
+++ e2e/tests/auth.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+import signIn from 'api/auth/sign-in/response';
+
+test('Sign-in', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' });
+
+  expect(page.getByText('Sign in to our platform')).toBeVisible();
+
+  await page.route('**/api/auth/sign-in', (route) =>
+    route.fulfill({
+      status: 200,
+      body: JSON.stringify(signIn['successful']),
+    }),
+  );
+
+  await page.getByTestId('sign-in').click();
+
+  await expect(page).toHaveURL(/.*dashboard/);
+});
