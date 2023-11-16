322995a0cec7a39911099d70e37f803cc6b2b688
Shyam-Chen <shyamchen1994@gmail.com>
2023-11-16 21:34:03 +0800

    1,211th Commit

--- src/routes/(backstage)/(library)/charts/world-map/WorldMap.vue
+++ src/routes/(backstage)/(library)/charts/world-map/WorldMap.vue
@@ -5,6 +5,35 @@ import * as d3 from 'd3';
 const worldMap = ref();
 const loading = ref(true);
 
+const color = [
+  '#313695', //    0
+  '#4575b4', //  100
+  '#74add1', //  200
+  '#abd9e9', //  300
+  '#e0f3f8', //  400
+  '#ffffbf', //  500
+  '#fee090', //  600
+  '#fdae61', //  700
+  '#f46d43', //  800
+  '#d73027', //  900
+  '#a50026', // 1000
+];
+
+const dataset = [
+  { name: 'Australia', value: getRandomInt() },
+  { name: 'Canada', value: getRandomInt() },
+  { name: 'China', value: getRandomInt() },
+  { name: 'England', value: getRandomInt() },
+  { name: 'France', value: getRandomInt() },
+  { name: 'Germany', value: getRandomInt() },
+  { name: 'India', value: getRandomInt() },
+  { name: 'Japan', value: getRandomInt() },
+  { name: 'Russia', value: getRandomInt() },
+  { name: 'South Korea', value: getRandomInt() },
+  { name: 'Taiwan', value: getRandomInt() },
+  { name: 'USA', value: getRandomInt() },
+];
+
 onMounted(async () => {
   loading.value = true;
 
@@ -21,6 +50,12 @@ onMounted(async () => {
   const width = 688;
   const height = 344;
 
+  const zoom: any = d3.zoom().on('zoom', zoomed);
+
+  function zoomed(event: any) {
+    svg.selectAll('g').attr('transform', event.transform);
+  }
+
   // append the svg object to the body of the page
   const svg = d3
     .select(worldMap.value)
@@ -29,7 +64,8 @@ onMounted(async () => {
     .attr('height', '100%')
     .attr('viewBox', '0 0 688 344')
     .attr('preserveAspectRatio', 'xMinYMin')
-    .style('position', 'relative');
+    .style('position', 'relative')
+    .call(zoom); // Enable zoom on the main SVG
 
   const gfg = d3
     .geoEquirectangular()
@@ -50,16 +86,33 @@ onMounted(async () => {
     .data(geojson.features)
     .enter()
     .append('path')
-    .attr('fill', '#71717a')
+    .attr('fill', function (d: any) {
+      const current = dataset.find((item) => item.name === d.properties.name);
+
+      if (current?.value) {
+        // Define your color scale and domain
+        const colorScale = d3
+          .scaleThreshold<number, string>()
+          .domain([100, 200, 300, 400, 500, 600, 700, 800, 900, 1000])
+          .range(color);
+
+        // Get the color based on the value
+        return colorScale(current.value) || '#a1a1aa';
+      }
+
+      return '#a1a1aa';
+    })
     .attr('d', d3.geoPath().projection(gfg) as any)
     .style('stroke', '#fafafa')
     .on('mouseover', function () {
       tooltip.style('opacity', 0.8).style('display', 'block');
       d3.select(this).style('opacity', 0.5);
     })
-    .on('mousemove', function (event) {
+    .on('mousemove', function (event, d: any) {
+      const current = dataset.find((item) => item.name === d.properties.name);
+
       tooltip
-        .html(`TODO`)
+        .html(`${d.properties.name}<br>Population: ${current?.value || 'N/A'}`)
         .style('top', event.pageY - 10 + 'px')
         .style('left', event.pageX + 10 + 'px');
     })
@@ -67,7 +120,52 @@ onMounted(async () => {
       tooltip.style('opacity', 0);
       d3.select(this).style('opacity', 1);
     });
+
+  // Create an SVG for the legend
+  const legend = svg.append('g').attr('transform', 'translate(10,' + (height - 30) + ')');
+
+  // Define the legend scale
+  const legendScale = d3
+    .scaleOrdinal<number, string>()
+    .domain([0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000])
+    .range(color);
+
+  // Create legend rectangles
+  legend
+    .selectAll('rect')
+    .data(legendScale.domain())
+    .enter()
+    .append('rect')
+    .attr('x', function (d, i) {
+      return i * 25; // Adjust the spacing as needed
+    })
+    .attr('width', 25)
+    .attr('height', 15)
+    .attr('fill', function (d) {
+      return legendScale(d) || '#a1a1aa';
+    });
+
+  // Create legend labels
+  legend
+    .selectAll('text')
+    .data(legendScale.domain())
+    .enter()
+    .append('text')
+    .attr('x', function (d, i) {
+      return i * 25 + 10; // Adjust the spacing and positioning as needed
+    })
+    .attr('y', 30) // Adjust the vertical position as needed
+    .text(function (d) {
+      return d;
+    })
+    .style('text-anchor', 'middle')
+    .style('alignment-baseline', 'middle')
+    .style('fill', '#fff'); // Adjust the text color as needed
 });
+
+function getRandomInt() {
+  return Math.floor(Math.random() * 1_000) + 1;
+}
 </script>
 
 <template>
