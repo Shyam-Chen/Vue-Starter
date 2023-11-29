f0344c351cff6f8c090acc5a4108f731e86a9fbf
Shyam-Chen <shyamchen1994@gmail.com>
2023-11-29 09:52:18 +0800

    1,228th Commit

--- ui/src/components/panel/Panel.vue
+++ ui/src/components/panel/Panel.vue
@@ -1,38 +1,61 @@
 <script lang="ts" setup>
-import { reactive } from 'vue';
+import { computed, reactive } from 'vue';
 
 import Collapse from '../collapse/Collapse.vue';
 
-defineProps<{
-  title?: string;
-  value?: string;
+const props = withDefaults(
+  defineProps<{
+    modelValue?: boolean;
+    title?: string;
+  }>(),
+  {
+    modelValue: undefined,
+    title: '',
+  },
+);
+
+const emit = defineEmits<{
+  (evt: 'update:modelValue', val: boolean): void;
 }>();
+
+const panelModel = computed({
+  get: () => props.modelValue || false,
+  set: (val) => emit('update:modelValue', val),
+});
 
 const flux = reactive({
   status: true,
   toggle() {
-    flux.status = !flux.status;
+    if (typeof props.modelValue === 'boolean') {
+      panelModel.value = !panelModel.value;
+    } else {
+      flux.status = !flux.status;
+    }
   },
 });
+
+const _status = computed(() =>
+  typeof props.modelValue === 'boolean' ? panelModel.value : flux.status,
+);
 </script>
 
 <template>
   <div class="w-full rounded">
     <div
       class="flex px-4 py-3 cursor-pointer shadow bg-white dark:bg-slate-800"
-      :class="{ 'accordion-active': flux.status }"
+      :class="{ 'accordion-active': _status }"
       @click="flux.toggle"
     >
       <div class="flex-1">
         <slot name="header">{{ title }}</slot>
       </div>
 
-      <div v-if="!flux.status" class="i-ic-baseline-arrow-drop-down w-6 h-6"></div>
+      <div v-if="!_status" class="i-ic-baseline-arrow-drop-down w-6 h-6"></div>
       <div v-else class="i-ic-baseline-arrow-drop-up w-6 h-6"></div>
     </div>
 
     <Collapse>
-      <div v-show="flux.status" class="shadow bg-white dark:bg-slate-800">
+      <div v-show="_status" class="shadow bg-white dark:bg-slate-800">
         <div class="p-4">
           <slot name="content"></slot>
         </div>
