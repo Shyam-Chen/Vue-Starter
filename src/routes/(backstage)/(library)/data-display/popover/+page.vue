e3ee13a639583958e256cc860f1843564bb74243
Shyam-Chen <shyamchen1994@gmail.com>
2023-11-30 10:10:18 +0800

    1,231st Commit

--- src/routes/(backstage)/(library)/data-display/popover/+page.vue
+++ src/routes/(backstage)/(library)/data-display/popover/+page.vue
@@ -1,20 +1,75 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
 import { XBreadcrumb, XPopover, XButton } from '@x/ui';
+
+const controlled = ref(false);
 </script>
 
 <template>
   <XBreadcrumb :items="[{ text: 'Library' }, { text: 'Data Display' }, { text: 'Popover' }]" />
 
   <div class="my-4">
-    <div class="text-3xl font-bold">Popover</div>
+    <div class="text-4xl font-extrabold">Popover</div>
   </div>
 
-  <div class="flex flex-col border p-4 mb-4">
-    <div class="mb-2">Basic</div>
+  <div class="my-8">
+    <div class="text-3xl font-bold my-4">Basic</div>
 
-    <div class="flex justify-center items-center gap-15">
+    <XPopover>
+      <XButton icon="i-mdi-atom" />
+
+      <template #content>
+        <div class="px-4 py-2">The content of the Popover.</div>
+      </template>
+    </XPopover>
+  </div>
+
+  <div class="my-8">
+    <div class="text-3xl font-bold mb-4 pt-6">Controlled</div>
+
+    <div class="flex">
+      Status:
+      <div>
+        <code class="text-sm text-gray-700 bg-gray-300 ml-2 px-2 py-1 rounded">
+          {{ controlled }}
+        </code>
+      </div>
+    </div>
+
+    <XPopover v-model="controlled">
+      <XButton icon="i-mdi-atom" />
+
+      <template #content>
+        <div class="flex px-4 py-2">
+          Status:
+          <div>
+            <code class="text-sm text-gray-700 bg-gray-300 ml-2 px-2 py-1 rounded">
+              {{ controlled }}
+            </code>
+          </div>
+        </div>
+      </template>
+    </XPopover>
+  </div>
+
+  <div class="my-8">
+    <div class="text-3xl font-bold my-4 pt-6">Disabled</div>
+
+    <XPopover disabled>
+      <XButton icon="i-mdi-atom" disabled />
+
+      <template #content>
+        <div class="px-4 py-2">The content of the Popover.</div>
+      </template>
+    </XPopover>
+  </div>
+
+  <div class="my-8">
+    <div class="text-3xl font-bold my-4 pt-6">Placement</div>
+
+    <div class="flex justify-end">
       <XPopover>
-        <XButton icon="i-fa-envelope" />
+        <XButton icon="i-mdi-atom" />
 
         <template #content>
           <div class="px-4 py-2">The content of the Popover.</div>
--- ui/src/components/popover/Popover.vue
+++ ui/src/components/popover/Popover.vue
@@ -1,10 +1,11 @@
 <script lang="ts" setup>
-import { ref, computed, reactive, nextTick } from 'vue';
+import { nextTick, ref, computed, reactive } from 'vue';
 import { vOnClickOutside } from '@vueuse/components';
+
+import Fade from '../fade/Fade.vue';
 
 const props = defineProps<{
   modelValue?: boolean;
-  options?: string[];
   disabled?: boolean;
 }>();
 
@@ -12,85 +13,73 @@ const emit = defineEmits<{
   (evt: 'update:modelValue', val: boolean): void;
 }>();
 
-const status = computed({
+const model = computed({
   get: () => props.modelValue || false,
   set: (val) => emit('update:modelValue', val),
 });
 
 const target = ref();
-const dropdown = ref();
+const panel = ref();
 
 const flux = reactive({
   status: false,
-  onMouseenter() {
+  toggle() {
     if (props.disabled) return;
 
     flux.status = !flux.status;
-    status.value = !status.value;
+    model.value = !model.value;
 
-    if (!flux.status && !status.value) return;
+    if (!flux.status && !model.value) return;
 
     nextTick(() => {
       const rect = target.value.getBoundingClientRect();
 
       const center = window.innerHeight / 2;
 
-      dropdown.value.style.left = `${rect.x}px`;
-      dropdown.value.style.top = `${rect.y}px`;
+      if (panel.value) {
+        panel.value.style.left = `${rect.x}px`;
+        panel.value.style.top = `${rect.y}px`;
 
-      const xAxis = `calc(${rect.width / 2}px - 50%)`;
+        const xAxis = `calc(${rect.width / 2}px - 50%)`;
 
-      if (rect.top > center) {
-        const yAxis = `calc(-100% - 0.25rem)`;
-        dropdown.value.style.transform = `translate(${xAxis}, ${yAxis})`;
-      } else {
-        const yAxis = `calc(${rect.height}px + 0.25rem)`;
-        dropdown.value.style.transform = `translate(${xAxis}, ${yAxis})`;
+        if (rect.top > center) {
+          const yAxis = `calc(-100% - 0.25rem)`;
+          panel.value.style.transform = `translate(${xAxis}, ${yAxis})`;
+        } else {
+          const yAxis = `calc(${rect.height}px + 0.25rem)`;
+          panel.value.style.transform = `translate(${xAxis}, ${yAxis})`;
+        }
       }
     });
   },
-  onMouseleave() {
+  close() {
     flux.status = false;
-    status.value = false;
+    model.value = false;
   },
 });
 </script>
 
 <template>
-  <div v-on-click-outside="flux.onMouseleave" class="relative inline-flex">
+  <div v-on-click-outside="flux.close" class="relative inline-flex">
     <div
       ref="target"
       class="inline-flex w-full justify-center items-center rounded-md"
-      @click="flux.onMouseenter"
+      @click="flux.toggle"
     >
       <slot></slot>
     </div>
 
-    <Transition
-      enter-active-class="transition-opacity duration-300 ease-out"
-      enter-from-class="opacity-0"
-      enter-to-class="opacity-100"
-      leave-active-class="duration-200 ease-in"
-      leave-from-class="opacity-100"
-      leave-to-class="opacity-0"
-    >
+    <Fade>
       <div
-        v-if="flux.status || status"
-        ref="dropdown"
-        class="fixed z-10 top-0 left-0 min-w-max bg-white dark:bg-slate-800 rounded-lg shadow-lg"
+        v-if="flux.status || model"
+        ref="panel"
+        class="fixed z-100 top-0 left-0 min-w-max bg-white dark:bg-slate-800 rounded-lg shadow-lg"
         tabindex="-1"
       >
         <div class="py-1">
           <slot name="content"> </slot>
         </div>
       </div>
-    </Transition>
+    </Fade>
   </div>
 </template>
-
-<style lang="scss" scoped>
-.option {
-  @apply px-3 py-1 cursor-pointer rounded-md;
-  @apply hover:text-primary-500 dark:hover:text-primary-100 hover:bg-primary-100 dark:hover:bg-primary-600;
-}
-</style>
