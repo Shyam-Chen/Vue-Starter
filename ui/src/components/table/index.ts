781cd542bf8c5e425a038c705bc9ab26d372ec24
Shyam-Chen <shyamchen1994@gmail.com>
2023-12-31 15:25:56 +0800

    1,294th Commit

--- ui/src/components/table/index.ts
+++ ui/src/components/table/index.ts
@@ -3,15 +3,8 @@ import Column from './Column.vue';
 import Row from './Row.vue';
 import Cell from './Cell.vue';
 
-// @ts-ignore
-Table.Column = Column;
-// @ts-ignore
-Table.Row = Row;
-// @ts-ignore
-Table.Cell = Cell;
-
-export default Table as typeof Table & {
-  Column: typeof Column;
-  Row: typeof Row;
-  Cell: typeof Cell;
-};
+export default Object.assign(Table, {
+  Column,
+  Row,
+  Cell,
+});
