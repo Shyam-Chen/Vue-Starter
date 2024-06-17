b3cabbe7ab6841f1ff4d9fb5181c3f981fa4c08a
Shyam-Chen <shyamchen1994@gmail.com>
2024-06-17 14:32:36 +0800

    1,483rd Commit

--- app/src/routes/(backstage)/(library)/network/eventsource/+page.vue
+++ app/src/routes/(backstage)/(library)/network/eventsource/+page.vue
@@ -1,8 +1,9 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { XBreadcrumb, XCard, XButton, XTextField } from '@x/ui';
 import { useEventSource } from '@x/ui';
 import { request } from '@x/ui';
+import { stream } from 'fetch-event-stream';
 
 const { data } = useEventSource('/sse');
 const { data: dataEvent } = useEventSource('/sse/event');
@@ -15,6 +16,16 @@ const doFetch = async () => {
     body: { message: message.value },
   });
 };
+
+const eventData = ref<string>();
+
+onMounted(async () => {
+  const events = await stream('/api/sse');
+
+  for await (let event of events) {
+    eventData.value = event.data;
+  }
+});
 </script>
 
 <template>
@@ -42,6 +53,16 @@ const doFetch = async () => {
           <XTextField v-model:value="message" />
         </div>
         <pre v-if="dataEvent">{{ JSON.parse(dataEvent) }}</pre>
+      </div>
+    </XCard>
+  </section>
+
+  <section class="my-8">
+    <h2 class="text-3xl font-bold my-4">Fetch</h2>
+
+    <XCard>
+      <div class="flex justify-center">
+        <div>{{ eventData }}</div>
       </div>
     </XCard>
   </section>
