03d25bfa7b6f5ef6655d7a776a73fb9780971c23
Shyam-Chen <shyamchen1994@gmail.com>
2024-07-03 10:27:08 +0800

    1,499th Commit

---
+++ eslint.config.js
@@ -0,0 +1,92 @@
+// @ts-check
+
+import globals from 'globals';
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import vue from 'eslint-plugin-vue';
+import vueParser from 'vue-eslint-parser';
+import prettier from 'eslint-plugin-prettier/recommended';
+
+export default ts.config(
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parser: vueParser,
+      parserOptions: {
+        parser: ts.parser,
+        sourceType: 'module',
+        extraFileExtensions: ['.vue'],
+      },
+    },
+  },
+
+  // js
+  js.configs.recommended,
+  {
+    rules: {
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+    },
+  },
+
+  // ts
+  ...ts.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+
+  // vue
+  ...vue.configs['flat/recommended'],
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/attribute-hyphenation': ['error', 'never'],
+      'vue/v-on-event-hyphenation': ['error', 'never'],
+      'vue/no-v-html': 'off',
+
+      'vue/block-lang': ['error', { script: { lang: 'ts' } }],
+      'vue/block-order': ['error', { order: ['script[setup]', 'template', 'style[scoped]'] }],
+      'vue/component-api-style': ['error', ['script-setup']],
+      'vue/component-name-in-template-casing': 'error',
+      'vue/custom-event-name-casing': 'error',
+      'vue/define-emits-declaration': 'error',
+      'vue/define-macros-order': [
+        'error',
+        {
+          order: ['defineOptions', 'defineModel', 'defineProps', 'defineEmits', 'defineSlots'],
+          defineExposeLast: true,
+        },
+      ],
+      'vue/define-props-declaration': 'error',
+      'vue/html-button-has-type': 'error',
+      'vue/no-multiple-objects-in-class': 'warn',
+      'vue/no-restricted-call-after-await': 'error',
+      'vue/no-template-target-blank': 'error',
+      'vue/no-unused-refs': 'warn',
+      'vue/no-use-v-else-with-v-for': 'error',
+      'vue/no-v-text': 'error',
+      'vue/padding-line-between-blocks': 'warn',
+      'vue/prefer-define-options': 'error',
+      'vue/prefer-separate-static-class': 'error',
+      'vue/prefer-true-attribute-shorthand': 'warn',
+      'vue/require-macro-variable-name': 'error',
+      'vue/require-typed-ref': 'warn',
+      'vue/v-for-delimiter-style': 'error',
+      'vue/valid-define-options': 'error',
+    },
+  },
+
+  // prettier
+  prettier,
+  {
+    rules: {
+      'prettier/prettier': 'warn',
+    },
+  },
+);
