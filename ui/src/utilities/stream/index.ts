45681167f463280ba6042e3835c563ffcae55423
Shyam-Chen <shyamchen1994@gmail.com>
2024-09-26 16:03:15 +0800

    1,560th Commit

--- ui/src/utilities/stream/index.ts
+++ ui/src/utilities/stream/index.ts
@@ -1,4 +1,4 @@
 import { stream } from 'fetch-event-stream';
 
 export default (url: string, init?: RequestInit) =>
-  stream(`${process.env.API_URL}/api/${url}`, init);
+  stream(`${process.env.API_URL}/api${url}`, init);
