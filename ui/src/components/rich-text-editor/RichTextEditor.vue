8715e77b9037dbc4f13b2a7896641c81aab900c4
Shyam-Chen <shyamchen1994@gmail.com>
2025-10-16 16:41:52 +0800

    1,634th Commit

--- ui/src/components/rich-text-editor/RichTextEditor.vue
+++ ui/src/components/rich-text-editor/RichTextEditor.vue
@@ -1,11 +1,7 @@
 <script lang="ts" setup>
 import Blockquote from '@tiptap/extension-blockquote';
 import Bold from '@tiptap/extension-bold';
-import BulletList from '@tiptap/extension-bullet-list';
-import Color from '@tiptap/extension-color';
 import Document from '@tiptap/extension-document';
-import Dropcursor from '@tiptap/extension-dropcursor';
-import Gapcursor from '@tiptap/extension-gapcursor';
 import HardBreak from '@tiptap/extension-hard-break';
 import Heading from '@tiptap/extension-heading';
 import Highlight from '@tiptap/extension-highlight';
@@ -14,14 +10,14 @@ import HorizontalRule from '@tiptap/extension-horizontal-rule';
 import Image from '@tiptap/extension-image';
 import Italic from '@tiptap/extension-italic';
 import Link from '@tiptap/extension-link';
-import ListItem from '@tiptap/extension-list-item';
-import OrderedList from '@tiptap/extension-ordered-list';
+import { BulletList, ListItem, OrderedList } from '@tiptap/extension-list';
 import Paragraph from '@tiptap/extension-paragraph';
 import Strike from '@tiptap/extension-strike';
 import Text from '@tiptap/extension-text';
 import TextAlign from '@tiptap/extension-text-align';
-import TextStyle from '@tiptap/extension-text-style';
+import { TextStyleKit } from '@tiptap/extension-text-style';
 import Underline from '@tiptap/extension-underline';
+import { Dropcursor, Gapcursor } from '@tiptap/extensions';
 import type { Transaction } from '@tiptap/pm/state';
 import type { Extensions } from '@tiptap/vue-3';
 import { Editor, EditorContent } from '@tiptap/vue-3';
@@ -79,9 +75,8 @@ onMounted(() => {
       Heading,
       Paragraph,
       Text,
-      TextStyle,
+      TextStyleKit,
 
-      Color,
       Highlight.configure({ multicolor: true }),
 
       Bold,
@@ -92,7 +87,7 @@ onMounted(() => {
       ListItem,
       BulletList,
       OrderedList,
-      TextAlign.configure({ types: ['heading', 'paragraph'] }),
+      TextAlign.configure({ types: ['heading', 'paragraph'], defaultAlignment: 'left' }),
 
       Image.configure({ inline: true }),
       Link,
