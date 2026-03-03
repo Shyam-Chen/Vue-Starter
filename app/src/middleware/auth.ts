4a9e4ef20ff87a7243850adf4f87e958f5c3f43e
Shyam-Chen <shyamchen1994@gmail.com>
2026-03-03 09:37:43 +0800

    1,643rd Commit

--- app/src/middleware/auth.ts
+++ app/src/middleware/auth.ts
@@ -1,6 +1,9 @@
+// import { request } from '@x/ui';
 import type { NavigationGuard } from 'vue-router';
 
-export default (() => {
+// import { Baseline, useBaselineStore } from '~/layouts/baseline';
+
+export default (async () => {
   if (!localStorage.getItem('accessToken')) {
     return { path: '/sign-in' };
   }
@@ -10,6 +13,15 @@ export default (() => {
   if (expiresIn && new Date().toISOString() >= expiresIn) {
     return { path: '/sign-in' };
   }
+
+  // const response = await request('/auth/user', { method: 'GET' });
+
+  // if (!response.ok) {
+  //   return { path: '/sign-in' };
+  // }
+
+  // const baselineStore = useBaselineStore();
+  // baselineStore.state.user = response._data;
 
   return true;
 }) as NavigationGuard;
